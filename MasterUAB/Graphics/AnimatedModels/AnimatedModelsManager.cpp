b43d814a1b73fbf639e9b98ea91d34d345df61cf
Ynozzar <aeyann@gmail.com>
2015-12-03 16:44:00 +0100

    AnimatedModelsManager en progreso.

--- MasterUAB/Graphics/AnimatedModels/AnimatedModelsManager.cpp
+++ MasterUAB/Graphics/AnimatedModels/AnimatedModelsManager.cpp
@@ -1,1 +1,46 @@
 #include "AnimatedModelsManager.h"
+#include "XML\XMLTreeNode.h"
+
+CAnimatedModelsManager::CAnimatedModelsManager(void)
+{
+}
+
+
+CAnimatedModelsManager::~CAnimatedModelsManager(void)
+{
+}
+
+bool CAnimatedModelsManager::Load(const std::string &FileName)
+{
+	m_Filename = FileName;
+	std::string l_AnimatedModelName;
+	std::string l_AnimatedModelPath;
+
+	CXMLTreeNode l_XML;
+	if (l_XML.LoadFile(FileName.c_str()))
+	{
+		CXMLTreeNode l_Input = l_XML["animated_models"];
+		if (l_Input.Exists())
+		{
+			for (int i = 0; i < l_Input.GetNumChildren(); ++i)
+			{
+				CXMLTreeNode l_Element = l_Input(i);
+				if (l_Element.GetName() == std::string("animated_model"))
+				{
+					l_AnimatedModelName = l_Element.GetPszProperty("name");
+					l_AnimatedModelPath = l_Element.GetPszProperty("path");
+				}
+			}
+		}
+	}
+	else
+	{
+		return false;
+	}
+	return true;
+}
+
+bool CAnimatedModelsManager::Reload()
+{
+	
+}
