de52adda7fe07fc500557eaf95d86bb075f30806
jonathanhr <jonathan.hortelano@gmail.com>
2015-12-06 22:54:20 +0100

    add load lights

---
+++ MasterUAB/Graphics/Lights/Light.cpp
@@ -0,0 +1,27 @@
+#include "Light.h"
+
+#include "XML\XMLTreeNode.h"
+#include "RenderManager\RenderManager.h"
+
+CLight::CLight() : CNamed(""){}
+
+CLight::CLight(CXMLTreeNode &TreeNode) : CNamed("")
+{
+	SetName(TreeNode.GetPszProperty("name"));
+	//SetType(TreeNode.GetPszProperty("type"));
+	SetPosition(TreeNode.GetVect3fProperty("pos", Vect3f(0.0f, 0.0f, 0.0f), true));
+	SetColor(CColor(TreeNode.GetVect4fProperty("color", Vect4f(255.0f, 255.0f, 255.0f, 0.0f), true)));
+	SetStartRangeAttenuation(TreeNode.GetFloatProperty("att_start_range"));
+	SetEndRangeAttenuation(TreeNode.GetFloatProperty("att_end_range"));
+	SetIntensity(TreeNode.GetFloatProperty("intensity"));
+}
+
+void CLight::Render(CRenderManager *RenderManager)
+{
+	RenderManager->Render();
+}
+
+/*TLightType CLight::GetLightTypeByName(const std::string &StrLightType)
+{
+	return m_Type[StrLightType];
+}*/
