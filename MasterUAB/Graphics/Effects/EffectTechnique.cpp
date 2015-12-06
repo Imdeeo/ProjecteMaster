3eb61ebcb8b6270730296014e7a9221d37fb217d
Imdeeo <joanillo2002@gmail.com>
2015-12-06 19:40:50 +0100

    CNamed constructor conTreeNode

--- MasterUAB/Graphics/Effects/EffectTechnique.cpp
+++ MasterUAB/Graphics/Effects/EffectTechnique.cpp
@@ -2,9 +2,8 @@
 #include "Engine\UABEngine.h"
 #include "XML\XMLTreeNode.h"
 
-CEffectTechnique::CEffectTechnique(CXMLTreeNode &TreeNode):CNamed("")
+CEffectTechnique::CEffectTechnique(CXMLTreeNode &TreeNode):CNamed(TreeNode)
 {
-	SetName(TreeNode.GetPszProperty("name"));
 	m_VertexShaderName = TreeNode.GetPszProperty("vertex_shader");
 	m_PixelShaderName = TreeNode.GetPszProperty("pixel_shader");
 	m_VertexShader = UABEngine.GetEffectManager()->GetVertexShader(m_VertexShaderName);
