0777ebed96945b6b8ec36dde0c0e2e80a0ed9985
Imdeeo <joanillo2002@gmail.com>
2015-12-07 01:34:07 +0100

    RenderableObjectmanager Reload implemented and activated AnimatedModelManager Reload activated

--- MasterUAB/Graphics/RenderableObjects/RenderableObjectManager.h
+++ MasterUAB/Graphics/RenderableObjects/RenderableObjectManager.h
@@ -4,26 +4,31 @@
 #include "TemplatedVectorMapManager.h"
 #include "RenderableObject.h"
 
+#include <string>
+
 class CXMLTreeNode;
 
 class CRenderableObjectsManager : public CTemplatedVectorMapManager<CRenderableObject>
 {
-	public:
-		CRenderableObjectsManager();
-		virtual ~CRenderableObjectsManager();
-		void Update(float ElapsedTime);
-		void Render(CRenderManager *RM);
-		CRenderableObject * AddMeshInstance(CXMLTreeNode &TreeNode);
-		CRenderableObject * AddMeshInstance(const std::string &CoreMeshName,
-			const std::string &InstanceName, const Vect3f &Position, const float _Yaw = 0.f,
-			const float _Pitch = 0.f, const float _Roll = 0.f,const float _Scale = 1.f,
-			const bool _Visible = true);
-		CRenderableObject * AddAnimatedInstanceModel(CXMLTreeNode &TreeNode);
-		CRenderableObject * AddAnimatedInstanceModel(const std::string &CoreModelName,
-			const std::string &InstanceModelName, const Vect3f &Position);
-		//void CleanUp();
-		bool Load(const std::string &FileName);
-		//CRenderableObject * GetInstance(const std::string &Name) const;
+private:
+	std::string m_Filename;
+public:
+	CRenderableObjectsManager();
+	virtual ~CRenderableObjectsManager();
+	void Update(float ElapsedTime);
+	void Render(CRenderManager *RM);
+	CRenderableObject * AddMeshInstance(CXMLTreeNode &TreeNode);
+	CRenderableObject * AddMeshInstance(const std::string &CoreMeshName,
+		const std::string &InstanceName, const Vect3f &Position, const float _Yaw = 0.f,
+		const float _Pitch = 0.f, const float _Roll = 0.f,const float _Scale = 1.f,
+		const bool _Visible = true);
+	CRenderableObject * AddAnimatedInstanceModel(CXMLTreeNode &TreeNode);
+	CRenderableObject * AddAnimatedInstanceModel(const std::string &CoreModelName,
+		const std::string &InstanceModelName, const Vect3f &Position);
+	//void CleanUp();
+	bool Reload();
+	bool Load(const std::string &FileName);
+	//CRenderableObject * GetInstance(const std::string &Name) const;
 };
 
 #endif
