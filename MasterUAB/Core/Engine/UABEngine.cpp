6bf8fe559f1559e8ba29b7a153ef05f1dd85b9c0
Ynozzar <aeyann@gmail.com>
2015-12-14 13:28:44 +0100

    Iniciazación del Lua. Cambiado el ifndef de mi_puto_engine_h a uabengine_h

--- MasterUAB/Core/Engine/UABEngine.cpp
+++ MasterUAB/Core/Engine/UABEngine.cpp
@@ -40,4 +40,6 @@ void CUABEngine::Init()
 	m_LightManager->Load("Data\\level_1\\lights.xml");
 	m_AnimatedModelsManager->Load("Data\\level_1\\animated_models.xml");
 	m_RenderableObjectsManager->Load("Data\\level_1\\renderable_objects.xml");
+	m_ScriptManager->Initialize();
+	m_ScriptManager->RegisterLUAFunctions();
 }
