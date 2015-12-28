13bea51459d07073ffe504f9e3ba8586706dcb42
Oriol <nah_one@hotmail.com>
2015-12-28 17:41:02 +0100

    Mejorado TemplatedVectorMapManager (arreglados algunos memoryleaks)

--- MasterUAB/Base/TemplatedVectorMapManager.h
+++ MasterUAB/Base/TemplatedVectorMapManager.h
@@ -18,8 +18,11 @@ public:
 		CMapResourceValue(){assert(!"Method must not be called");}
 		CMapResourceValue(T *Value, size_t Id) : m_Value(Value), m_Id(Id){}
 	};
+
 	typedef std::vector<T *> TVectorResources;
-	typedef std::map<std::string, CMapResourceValue*> TMapResources;
+	typedef std::map<std::string, CMapResourceValue> TMapResources;
+	
+
 
 protected:
 	TVectorResources m_ResourcesVector;
@@ -40,7 +43,7 @@ public:
 		delete m_ResourcesMap[Name];
 		m_ResourcesMap.erase(Name);
 		m_ResourcesVector.erase(m_ResourcesVector.begin()+index);
-		for(TMapResources::iterator l_iterator = m_ResourcesMap.begin();l_iterator != m_ResourcesMap.end();l_iterator++)
+		for (TMapResources::iterator l_iterator = m_ResourcesMap.begin(); l_iterator != m_ResourcesMap.end(); l_iterator++)
 		{
 			if(l_iterator->second->m_Id>index)
 			{
@@ -57,16 +60,17 @@ public:
 
 	virtual T * GetResource(const std::string &Name)
 	{
-		return m_ResourcesMap[Name]->m_Value;
+		return m_ResourcesMap[Name].m_Value;
 	}
 
 	virtual bool AddResource(const std::string &Name, T *Resource)
 	{
-		if(m_ResourcesMap.find(Name)!=m_ResourcesMap.end())
+		if (m_ResourcesMap.find(Name) != m_ResourcesMap.end())
 		{
 			return false;
 		}
-		m_ResourcesMap[Name] = new CMapResourceValue (Resource,m_ResourcesVector.size());
+
+		m_ResourcesMap.insert(std::pair<std::string, CMapResourceValue>(Name, CMapResourceValue(Resource, m_ResourcesVector.size())));
 		m_ResourcesVector.push_back(Resource);
 		return true;
 	}
@@ -77,10 +81,7 @@ public:
 		{
 			delete m_ResourcesVector[i];
 		}
-		for(TMapResources::iterator l_iterator = m_ResourcesMap.begin();l_iterator != m_ResourcesMap.end();l_iterator++)
-		{
-			delete l_iterator->second;
-		}
+
 		m_ResourcesMap.clear();
 		m_ResourcesVector.clear();
 	}
