156432ee3ff964f51971aa0fb609e1a4f37b68d5
Ynozzar <aeyann@gmail.com>
2015-12-29 20:37:03 +0100

    CameraInfo, CameraKey, CameraKeyController implementados. Falta getCurrentKey y Update.

---
+++ MasterUAB/Graphics/Camera/CameraInfo.h
@@ -0,0 +1,19 @@
+#ifndef CAMERA_INFO_H
+#define CAMERA_INFO_H
+
+#include "Math\Vector3.h"
+#include "XML\XMLTreeNode.h"
+
+class CCameraInfo
+{	
+public:
+	float m_NearPlane, m_FarPlane, m_FOV;
+	Vect3f m_Eye, m_LookAt, m_Up;
+
+	CCameraInfo();
+	CCameraInfo(const Vect3f &Eye, const Vect3f &LookAt, const Vect3f &Up, float NearPlane, float FarPlane, float FOV);
+	CCameraInfo(CXMLTreeNode &XMLTreeNode);
+	virtual ~CCameraInfo();
+};
+
+#endif
