caadcca6f4d099a6d7026ef8bb44019f3b375ba4
Imdeeo <joanillo2002@gmail.com>
2016-01-13 21:26:53 +0100

    Continuando conPhysXManager definicion de PhysXManagerImplementation

--- MasterUAB/PhysX/PhysXManager.cpp
+++ MasterUAB/PhysX/PhysXManager.cpp
@@ -1,1 +1,52 @@
 #include "PhysXManager.h"
+#include "Math\Vector3.h"
+#include "Math\Quatn.h"
+
+
+#include <PxPhysicsAPI.h>
+
+static physx::PxDefaultErrorCallback gDefaultErrorCallback;
+static physx::PxDefaultAllocator gDefaultAllocator;
+
+#if USE_PHYSX_DEBUG
+#define PVD_HOST "127.0.0.1"
+#endif
+
+#ifdef CHECKED_RELEASE
+#undef CHECKED_RELEASE
+#endif
+#define CHECKED_RELEASE(x) if(x!=nullptr) {x->release();x=nullptr;}
+
+
+inline physx::PxVec3 CastVec(const Vect3f& v)
+{ return physx::PxVec3(v.x,v.y,v.z);}
+inline Vect3f CastVec(const physx::PxVec3& v)
+{ return Vect3f(v.x,v.y,v.z);}
+inline Vect3f CastVec(const physx::PxExtendedVec3& v)
+{ return Vect3f((float)v.x,(float)v.y,(float)v.z);}
+
+inline physx::PxQuat CastQuat(const Quatf& q)
+{ return physx::PxQuat(q.x,q.y,q.z,q.w);}
+inline Quatf CastQuat(const physx::PxQuat& q)
+{ return Quatf(q.x,q.y,q.z,q.w);}
+
+
+
+class CPhysXManagerImplementation:
+	public CPhysXManager,
+	public physx::PxSimulationEventCallback, 
+	public physx::PxUserControllerHitReport
+{
+public:
+	CPhysXManagerImplementation();
+
+	void onConstraintBreak(physx::PxConstraintInfo* constraints, physx::PxU32 count){}
+	void onWake(physx::PxActor** actors, physx::PxU32 count){}
+	void onSleep(physx::PxActor** actors, physx::PxU32 count){}
+	void onContact(const physx::PxContactPairHeader& pairHeader,const physx::PxContactPair* pairs, physx::PxU32 nbPairs){}
+	void onTrigger(physx::PxTriggerPair* pairs, physx::PxU32 count){};
+
+	void onShapeHit(const physx::PxControllerShapeHit& hit){}
+	void onControllerHit(const physx::PxControllerHit& hit){}
+	void onObstacleHit(const physx::PxControllerObstacleHit& hit){}
+};
--- MasterUAB/PhysX/PhysXManager.h
+++ MasterUAB/PhysX/PhysXManager.h
@@ -20,3 +20,29 @@ namespace physx
 
 	typedef debugger::comm::PvdConnection PxVisualDebuggerConnection;
 }
+
+#define USE_PHYSX_DEBUG 0
+
+class CPhysXManager
+{
+protected:
+	CPhysXManager();
+public:
+	static CPhysXManager* CreatePhysXManager();
+	virtual ~CPhysXManager();
+
+protected:
+
+	physx::PxFoundation				*m_Foundation;
+	physx::PxPhysics				*m_Physics;
+
+#if USE_PHYSX_DEBUG
+	physx::PxVisualDebuggerConnection *m_DebugConnection;
+#endif
+
+	physx::PxDefaultCpuDispatcher	*m_Dispatcher;
+	physx::PxScene					*m_Scene;
+	physx::PxCooking				*m_Cooking;
+	physx::PxControllerManager		*m_ControllerManager;
+
+};
