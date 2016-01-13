e22f8509d6876f5bbf22f4435beb6a9ecc85697c
Imdeeo <joanillo2002@gmail.com>
2016-01-13 20:34:25 +0100

    PhysX Creado Manager Propiedades de Debug

---
+++ MasterUAB/PhysX/PhysXManager.h
@@ -0,0 +1,22 @@
+namespace physx
+{
+	class PxFoundation;
+	class PxPhysics;
+	class PxDefaultCpuDispatcher;
+	class PxScene;
+	class PxCooking;
+	class PxMaterial;
+	class Pxactor;
+	class PxControllerManager;
+	class PxController;
+
+	namespace debugger
+	{
+		namespace comm
+		{
+			class PvdConnection;
+		}
+	}
+
+	typedef debugger::comm::PvdConnection PxVisualDebuggerConnection;
+}
