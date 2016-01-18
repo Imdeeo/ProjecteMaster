b62c1a39a97b36ccf74309d3f16485c4ae20f8f2
Imdeeo <joanillo2002@gmail.com>
2016-01-18 20:57:01 +0100

    Crador de objetos dinamicos insertar objetos dinamicos

--- MasterUAB/PhysX/PhysXManager.h
+++ MasterUAB/PhysX/PhysXManager.h
@@ -1,3 +1,9 @@
+#include <map>
+#include <string>
+#include <vector>
+#include "Math\Vector4.h"
+#include "Math\Quatn.h"
+
 namespace physx
 {
 	class PxFoundation;
@@ -6,7 +12,7 @@ namespace physx
 	class PxScene;
 	class PxCooking;
 	class PxMaterial;
-	class Pxactor;
+	class PxActor;
 	class PxControllerManager;
 	class PxController;
 
@@ -31,6 +37,18 @@ public:
 	static CPhysXManager* CreatePhysXManager();
 	virtual ~CPhysXManager();
 
+	void RegisterMaterial(const std::string &name, float staticFriction, float dynamicFriction, float restitution);
+	inline void AssertCoordArrays();
+	inline size_t GetActorIndexFromName(const std::string& _actorName);
+
+	Vect3f GetActorPosition(const std::string& _actorName);
+	Quatf GetActorOrientation(const std::string& _actorName);
+
+	void GetActorPositionAndOrientation(const std::string& _actorName,Vect3f* Pos_, Quatf* Orienation_);
+
+	void CreateStaticShape(Vect3f _size,physx::PxMaterial &_Material,Vect3f _position, Quatf _orientation,size_t* index);
+	void CreateStaticPlane(Vect4f _size,physx::PxMaterial &_Material,Vect3f _position, Quatf _orientation,size_t* index);
+
 protected:
 
 	physx::PxFoundation				*m_Foundation;
@@ -44,5 +62,15 @@ protected:
 	physx::PxScene					*m_Scene;
 	physx::PxCooking				*m_Cooking;
 	physx::PxControllerManager		*m_ControllerManager;
+
+	std::map<std::string,physx::PxMaterial*> m_Materials;
+
+	std::map<std::string, size_t>	m_ActorIndexs;
+	std::vector<std::string>		m_ActorNames;
+	std::vector<Vect3f>				m_ActorPositions;
+	std::vector<Quatf>				m_ActorOrientations;
+	std::vector<physx::PxActor*>	m_Actors;
+	
+	void AddActor(std::string _actorName, Vect3f _position, Quatf _orientation, physx::PxActor*);
 
 };
