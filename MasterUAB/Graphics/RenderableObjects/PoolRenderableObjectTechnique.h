e96981d67c5b2a0af7d87ec230b51b48595de707
jonathanhr <jonathan.hortelano@gmail.com>
2016-01-19 21:25:47 +0100

    Generado PoolRenderableObjectTechnique, RenderableObjectTechnique y RenderableObjectTechniqueManager

---
+++ MasterUAB/Graphics/RenderableObjects/PoolRenderableObjectTechnique.h
@@ -0,0 +1,31 @@
+#ifndef H_POOLRENDERABLEOBJECTTECHNIQUE_H
+#define H_POOLRENDERABLEOBJECTTECHNIQUE_H
+
+#include "Utils\Named.h"
+#include "RenderableObjectTechnique.h"
+
+#include <vector>
+
+class CXMLTreeNode;
+
+class CPoolRenderableObjectTechnique : public CNamed
+{
+private:
+	class CPoolRenderableObjectTechniqueElement
+	{
+	public:
+		CRenderableObjectTechnique m_RenderableObjectTechnique;
+		CRenderableObjectTechnique *m_OnRenderableObjectTechniqueManager;
+		CPoolRenderableObjectTechniqueElement(const std::string &Name, CEffectTechnique *EffectTechnique, CRenderableObjectTechnique *OnRenderableObjectTechniqueManager):
+			m_RenderableObjectTechnique(Name,EffectTechnique), m_OnRenderableObjectTechniqueManager(OnRenderableObjectTechniqueManager){}
+	};
+	std::vector<CPoolRenderableObjectTechniqueElement *> m_RenderableObjectTechniqueElements;
+public:
+	CPoolRenderableObjectTechnique(CXMLTreeNode &TreeNode);
+	virtual ~CPoolRenderableObjectTechnique();
+	void Destroy();
+	void AddElement(const std::string &Name, const std::string &TechniqueName, CRenderableObjectTechnique *ROTOnRenderableObjectTechniqueManager);
+	void Apply();
+};
+
+#endif // H_POOLRENDERABLEOBJECTTECHNIQUE_H
