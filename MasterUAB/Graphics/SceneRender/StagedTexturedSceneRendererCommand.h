4cd95aa649d298ea6e1c945eaaa50d0e55f87db7
Imdeeo <joanillo2002@gmail.com>
2016-01-27 21:12:01 +0100

    CStagedTexturedSceneRendererCommand CDrawQuadRendererCommand

---
+++ MasterUAB/Graphics/SceneRender/StagedTexturedSceneRendererCommand.h
@@ -0,0 +1,45 @@
+#ifndef STAGED_TEXTURED_SCENE_RENDERER_COMMAND_H
+#define STAGED_TEXTURED_SCENE_RENDERER_COMMAND_H
+
+#include "SceneRender\SceneRendererCommand.h"
+#include "Texture\Texture.h"
+#include <vector>
+
+class CStagedTexturedSceneRendererCommand :
+	public CSceneRendererCommand
+{
+protected:
+	class CStagedTexture
+	{
+	public:
+		unsigned int m_StageId;
+		CTexture * m_Texture;
+
+		CStagedTexture(unsigned int _StageId, CTexture *_Texture)
+		{
+			m_StageId = _StageId;
+			m_Texture = _Texture;
+		}
+		void Activate()
+		{
+			m_Texture->Activate(m_StageId);
+		}
+	};
+
+	std::vector<CStagedTexture>				m_StagedTextures;
+	//std::vector<CDynamicTexture*>			m_DynamicTextures;
+	std::vector<ID3D11RenderTargetView*>	m_RenderTargetViews;
+
+public:
+	CStagedTexturedSceneRendererCommand(CXMLTreeNode & TreeNode);
+	virtual ~CStagedTexturedSceneRendererCommand(void);
+
+	void CreateRenderTargetViewVector();
+	void ActivateTextures();
+	void AddStageTexture(unsigned int _StageId, CTexture* _Texture);
+	virtual void Execute(CRenderManager &_RenderManager) = 0;
+
+
+};
+
+#endif //STAGED_TEXTURED_SCENE_RENDERER_COMMAND_H
