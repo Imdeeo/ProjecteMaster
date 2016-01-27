0cc868634fe3a20b62751e7fd6bf40e63d6e85ff
Imdeeo <joanillo2002@gmail.com>
2016-01-27 17:27:23 +0100

    SpotLight deffault angle and fall_off
    Light Default collor (_,_,_,1.f) ended with 1.f

--- MasterUAB/Graphics/Lights/SpotLight.cpp
+++ MasterUAB/Graphics/Lights/SpotLight.cpp
@@ -6,6 +6,6 @@ CSpotLight::CSpotLight():CDirectionalLight(),m_Angle(0.0f),m_FallOff(0.0f){}
 
 CSpotLight::CSpotLight(CXMLTreeNode &TreeNode) : CDirectionalLight(TreeNode)
 {
-	m_Angle = TreeNode.GetFloatProperty("angle");
-	m_FallOff = TreeNode.GetFloatProperty("fall_off");
+	m_Angle = TreeNode.GetFloatProperty("angle",1.f);
+	m_FallOff = TreeNode.GetFloatProperty("fall_off", 1.2f);
 }
