36dac8f6b9a1016258e649d5a12e08c1276f6806
Robb <trincasidra@users.noreply.github.com>
2016-02-10 12:46:31 +0100

    Comenzado a implementar el shadowmap.

--- MasterUAB/Graphics/Lights/DirectionalLight.cpp
+++ MasterUAB/Graphics/Lights/DirectionalLight.cpp
@@ -28,3 +28,25 @@ const Mat44f & CDirectionalLight::GetTransform()
 
 	return m_TransformMatrix;
 }
+/*
+void CDirectionalLight::SetShadowMap(CRenderManager &RenderManager)
+{
+	m_ViewShadowMap.SetIdentity();
+	m_ViewShadowMap.SetFromLookAt(m_Position, m_Position + m_Direction, v3fY);
+	unsigned int l_ShadowMapWidth = m_ShadowMap->GetWidth();
+	unsigned int l_ShadowMapHeight = m_ShadowMap->GetHeight();
+	m_ProjectionShadowMap.SetFromOrtho(m_OrthoShadowMapSize.x, m_OrthoShadowMapSize.y, 0.1f, m_EndRangeAttenuation);
+	CEffectManager::m_SceneParameters.m_View = m_ViewShadowMap;
+	CEffectManager::m_SceneParameters.m_Projection = m_ProjectionShadowMap;
+	ID3D11RenderTargetView *l_RenderTargetViews[1];
+	l_RenderTargetViews[0] = m_ShadowMap->GetRenderTargetView();
+	D3D11_VIEWPORT m_viewport;
+	m_viewport.Width = (float)l_ShadowMapWidth;
+	m_viewport.Height = (float)l_ShadowMapHeight;
+	m_viewport.MinDepth = 0.0f;
+	m_viewport.MaxDepth = 1.0f;
+	m_viewport.TopLeftX = 0.0f;
+	m_viewport.TopLeftY = 0.0f;
+	RenderManager.GetDeviceContext()->RSSetViewports(1, &m_viewport);
+	RenderManager.SetRenderTargets(1, l_RenderTargetViews, m_ShadowMap ->GetDepthStencilView());
+}*/
