2bfcfd564d5fd638ec6d4fb8ec104d507f32e30d
jonathanhr <jonathan.hortelano@gmail.com>
2016-02-11 16:58:57 +0100

    arreglados varios bugs de captureframebuffer anyadido applyfilter y captureframebuffer a scenerenderercommandmanager arreglago bug en stagedtexture para los captureframebuffer

--- MasterUAB/Graphics/SceneRender/ApplyFiltersSceneRendererCommand.cpp
+++ MasterUAB/Graphics/SceneRender/ApplyFiltersSceneRendererCommand.cpp
@@ -1,5 +1,7 @@
 #include "ApplyFiltersSceneRendererCommand.h"
 #include "RenderManager\RenderManager.h"
+#include "Texture\DynamicTexture.h"
+#include "Engine\UABEngine.h"
 
 CApplyFiltersSceneRendererCommand::CApplyFiltersSceneRendererCommand(CXMLTreeNode &TreeNode) : CStagedTexturedSceneRendererCommand(TreeNode)
 {
@@ -8,10 +10,14 @@ CApplyFiltersSceneRendererCommand::CApplyFiltersSceneRendererCommand(CXMLTreeNod
 
 void CApplyFiltersSceneRendererCommand::Execute(CRenderManager &_RenderManager)
 {
-	for (size_t i = 0; i < m_StagedTextures.size(); ++i)
-		m_StagedTextures[i].Activate();
+	CContextManager *l_ContextManager = _RenderManager.GetContextManager();
+	ID3D11DepthStencilView *l_DepthStencilView = m_DynamicTextures.empty() ? NULL : (m_DynamicTextures[0]->GetDepthStencilView());
 
 	for (size_t i = 0; i < m_DynamicTextures.size(); ++i)
 	{
+		if (i == 0)
+			m_StagedTextures[0].Activate();
+		
+		l_ContextManager->GetDeviceContext()->OMSetRenderTargets(1, &m_RenderTargetViews[i], l_DepthStencilView);
 	}
 }
