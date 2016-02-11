90af0974af4b419fcca85b0b54ae52daddc75bb3
Robb <trincasidra@users.noreply.github.com>
2016-02-11 21:22:01 +0100

    Shadowmap command.

--- MasterUAB/Graphics/SceneRender/GenerateShadowMapsSceneRendererCommand.cpp
+++ MasterUAB/Graphics/SceneRender/GenerateShadowMapsSceneRendererCommand.cpp
@@ -1,5 +1,6 @@
 #include "GenerateShadowMapsSceneRendererCommand.h"
 
+#include "Engine\UABEngine.h"
 #include "RenderManager\RenderManager.h"
 
 CGenerateShadowMapsSceneRendererCommand::CGenerateShadowMapsSceneRendererCommand(CXMLTreeNode &TreeNode) :CSceneRendererCommand(TreeNode)
@@ -13,17 +14,19 @@ CGenerateShadowMapsSceneRendererCommand::~CGenerateShadowMapsSceneRendererComman
 
 void CGenerateShadowMapsSceneRendererCommand::Execute(CRenderManager &_RenderManager)
 {
-	//TODO
-	//foreach light in lights, light->SetShadowMap();
-	/*Para ello estableceremos los valores de cámara según la luz, es decir crearemos
-	la matriz de View y de Projection a través de los parámetros de la luz en el
-	método SetShadowMap.
+	/*
+		por cada luz si está activada y genera shadowmap
+		Matrices view y projection de cada luz con el método SetShadowMap
+		SetRenderTarget
+		Clear Depth
+		Pintar las capas de luces
+	*/
 
-	Nos guardaremos las matrices de ShadowProjection y de ShadowView según
-	nuestras matrices de View y de Projection que serán utilizadas más tarde al
-	renderizar la luz que genera la sombra.
-
-	Una vez calculadas las matrices nos disponemos a renderizar la escena
-	estableciendo nuestra textura como RenderTarget. Renderizamos nuestra escena
-	del Proc y desestablecemos nuestra textura como RenderTarget.*/
+	int n_lights = UABEngine.GetLightManager()->GetResourcesVector().size();
+	for (size_t i = 0; i < n_lights; i++)
+	{
+		if(UABEngine.GetLightManager()->GetResourceById(i)->GetEnabled() && UABEngine.GetLightManager()->GetResourceById(i)->GetGenerateShadowMap()){
+			UABEngine.GetLightManager()->GetResourceById(i)->SetShadowMap(_RenderManager);
+		}
+	}
 }
