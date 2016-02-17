4be4500389596699ac6459c8f850b876c8b8c48d
Ynozzar <aeyann@gmail.com>
2016-02-17 14:30:33 +0100

    Implementada la visualización de shadowmap.... pero no funciona.

--- MasterUAB/Graphics/SceneRender/DeferredShadingSceneRendererCommand.cpp
+++ MasterUAB/Graphics/SceneRender/DeferredShadingSceneRendererCommand.cpp
@@ -21,12 +21,16 @@ void CDeferredShadingSceneRendererCommand::Execute(CRenderManager &_RenderManage
 	Vect4f v(1, 1, 1, 1);
 	_RenderManager.GetContextManager()->GetDeviceContext()->OMSetBlendState(m_EnabledAlphaBlendState, NULL, 0xffffffff);
 
+	int l_ShadowmapStageId = m_StagedTextures.size();
+
 	for (int i = 0; i < m_StagedTextures.size(); ++i)
 				m_StagedTextures[i].Activate();
 
 	for (int j = 0; j < UABEngine.GetLightManager()->GetResourcesVector().size(); ++j)
 	{
-		UABEngine.GetEffectManager()->SetLightConstants(0,UABEngine.GetLightManager()->GetResourceById(j));
+		CLight *l_Light = UABEngine.GetLightManager()->GetResourceById(j);
+		UABEngine.GetEffectManager()->SetLightConstants(0, l_Light);
+		l_Light->GetShadowMap()->Activate(l_ShadowmapStageId);
 		_RenderManager.DrawScreenQuad(m_RenderableObjectTechnique->GetEffectTechnique(), NULL, 0, 0, 1, 1, CColor(1.f, 1.f, 1.f, 1.f));
 	}
 	_RenderManager.GetContextManager()->GetDeviceContext()->OMSetBlendState(NULL, NULL, 0xffffffff);
