123b1fa11c3703d2c268d4086c94f61850b9065b
Ynozzar <aeyann@gmail.com>
2016-02-25 12:49:09 +0100

    Light Scattering casi casi

--- MasterUAB/Graphics/Camera/Camera.h
+++ MasterUAB/Graphics/Camera/Camera.h
@@ -101,6 +101,19 @@ public:
 			return CAMERA_TYPE_REVERSE;
 		return CAMERA_TYPE_NULL;
 	}
+
+	Vect2f CCamera::GetPositionInScreenCoordinates(const Vect3f &Position) const
+	{
+		Mat44f l_ViewProj = m_View*m_Projection;
+		Vect4f l_Pos4f(Position.x, Position.y, Position.z, 1.0);
+		float x = l_Pos4f.x*l_ViewProj.m00 + l_Pos4f.y*l_ViewProj.m10 +	l_Pos4f.z*l_ViewProj.m20 + l_ViewProj.m30;
+		float y = l_Pos4f.x*l_ViewProj.m01 + l_Pos4f.y*l_ViewProj.m11 +	l_Pos4f.z*l_ViewProj.m21 + l_ViewProj.m31;
+		float z = l_Pos4f.x*l_ViewProj.m02 + l_Pos4f.y*l_ViewProj.m12 +	l_Pos4f.z*l_ViewProj.m22 + l_ViewProj.m32;
+		float w = l_Pos4f.x*l_ViewProj.m03 + l_Pos4f.y*l_ViewProj.m13 +	l_Pos4f.z*l_ViewProj.m23 + l_ViewProj.m33;
+		if (w == 0)
+			w = 1;
+		return Vect2f(x / w, y / w);
+	}
 };
 
 #endif
