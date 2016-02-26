554b8c0ff85b00e25bdb690c8b8e2d97989de200
Robb <trincasidra@users.noreply.github.com>
2016-02-26 13:03:48 +0100

    Creadas clases de blend y blendop.

--- MasterUAB/Graphics/SceneRender/EnableAlphaBlendSceneRendererCommand.cpp
+++ MasterUAB/Graphics/SceneRender/EnableAlphaBlendSceneRendererCommand.cpp
@@ -1,49 +1,22 @@
 #include "EnableAlphaBlendSceneRendererCommand.h"
 
 #include "RenderManager\RenderManager.h"
-#include "Engine\UABEngine.h"
 
-CEnableAlphaBlendSceneRendererCommand::CEnableAlphaBlendSceneRendererCommand(CXMLTreeNode &TreeNode) :CSceneRendererCommand(TreeNode)
+CEnableAlphaBlendSceneRendererCommand::CEnableAlphaBlendSceneRendererCommand(CXMLTreeNode &TreeNode) :CSceneRendererCommand(TreeNode),
+	m_SrcBlend(TreeNode, "src"),
+	m_DestBlend(TreeNode, "dest"),
+	m_SrcAlphaBlend(TreeNode, "src_alpha"),					//C2614 illegal initialization
+	m_DestAlphaBlend(TreeNode, "dest_alpha"),
+	m_OpBlend(TreeNode, "operation"),
+	m_OpAlphaBlend(TreeNode, "operation_alpha")
 {
-	m_Blend[0] = CheckBlendType(TreeNode.GetPszProperty("operation"));
-	m_Blend[1] = CheckBlendType(TreeNode.GetPszProperty("src"));
-	m_Blend[2] = CheckBlendType(TreeNode.GetPszProperty("dest"));
-	m_Blend[3] = CheckBlendType(TreeNode.GetPszProperty("operation_alpha"));
-	m_Blend[4] = CheckBlendType(TreeNode.GetPszProperty("src_alpha"));
-	m_Blend[5] = CheckBlendType(TreeNode.GetPszProperty("dest_alpha"));
-
-	change the enable alpha blend command to enable any blend type instead of just standard
-use the ccamera::GetPositionInScreenCoordinates to calculate the directionallight in-screen position to use it as
-center for the light scattering
-
-<enable_alpha_blend
-	operation="additive"
-	src="one"
-	dest="one"
-	operation_alpha="additive"
-	src_alpha="one"
-	dest_alpha="one_minus_alpha"
-/>
-
-ZERO              = 1
-ONE               = 2
-SRC_COLOR         = 3
-INV_SRC_COLOR     = 4
-SRC_ALPHA         = 5
-INV_SRC_ALPHA     = 6
-DEST_ALPHA        = 7
-INV_DEST_ALPHA    = 8
-DEST_COLOR        = 9
-INV_DEST_COLOR    = 10
-SRC_ALPHA_SAT     = 11
-BLEND_FACTOR      = 14
-INV_BLEND_FACTOR  = 15
-SRC1_COLOR        = 16
-INV_SRC1_COLOR    = 17
-SRC1_ALPHA        = 18
-INV_SRC1_ALPHA    = 19
+	m_SrcBlend = CBlend(TreeNode, "src");
+	m_DestBlend = CBlend(TreeNode, "dest");
+	m_SrcAlphaBlend = CBlend(TreeNode, "src_alpha");		// C2065 identificador no declarado
+	m_DestAlphaBlend = CBlend(TreeNode, "dest_alpha");
+	m_OpBlend = CBlendOp(TreeNode, "operation");
+	m_OpAlphaBlend = CBlendOp(TreeNode, "operation_alpha");
 }
-
 
 CEnableAlphaBlendSceneRendererCommand::~CEnableAlphaBlendSceneRendererCommand()
 {
@@ -51,26 +24,63 @@ CEnableAlphaBlendSceneRendererCommand::~CEnableAlphaBlendSceneRendererCommand()
 
 void CEnableAlphaBlendSceneRendererCommand::Execute(CRenderManager &_RenderManager)
 {
-	_RenderManager.EnableBlendState(&m_Blend);
+	//_RenderManager.EnableBlendState(&m_Blend);
 }
 
-D3D11_BLEND CEnableAlphaBlendSceneRendererCommand::CheckBlendType(std::string _type)
-{	
-	if(_type == "zero"){ return D3D11_BLEND_ZERO; }
-	if(_type == "one"){ return D3D11_BLEND_ONE; }
-	if(_type == "src_color"){ return D3D11_BLEND_SRC_COLOR; }
-	if(_type == "inv_src_color"){ return D3D11_BLEND_SRC_ALPHA; }
-	if(_type == "src_alpha"){ return D3D11_BLEND_INV_SRC_COLOR; }
-	if(_type == "inv_src_alpha"){ return D3D11_BLEND_INV_SRC_ALPHA; }
-	if(_type == "dest_alpha"){ return D3D11_BLEND_DEST_ALPHA; }
-	if(_type == "inv_dest_alpha"){ return D3D11_BLEND_INV_DEST_ALPHA; }
-	if(_type == "dest_color"){ return D3D11_BLEND_DEST_COLOR; }
-	if(_type == "inv_dest_color"){ return D3D11_BLEND_INV_DEST_COLOR; }
-	if(_type == "src_alpha_sat"){ return D3D11_BLEND_SRC_ALPHA_SAT; }
-	if(_type == "blend_factor"){ return D3D11_BLEND_BLEND_FACTOR; }
-	if(_type == "inv_blend_factor"){ return D3D11_BLEND_INV_BLEND_FACTOR; }
-	if(_type == "src1_color"){ return D3D11_BLEND_SRC1_COLOR; }
-	if(_type == "inv_src1_color"){ return D3D11_BLEND_INV_SRC1_COLOR; }
-	if(_type == "src1_alpha"){ return D3D11_BLEND_SRC1_ALPHA; }
-	if(_type == "inv_src1_alpha"){ return D3D11_BLEND_INV_SRC1_ALPHA; }
+
+CBlend::CBlend(const CXMLTreeNode &TreeNode, const std::string _name):
+	CNamed(_name),
+	m_Blend(D3D11_BLEND_ONE)
+{
+	const char *charName = _name.c_str();
+	m_Blend = CheckBlendType(TreeNode.GetPszProperty(charName));
+}
+
+void CBlend::Destroy()
+{
+	// CHECKED_RELEASE(m_Blend); CHECKED_DESTROY(m_Blend); OJOCUIDAO
+}
+
+int CBlend::CheckBlendType(const std::string _type)
+{
+	if (_type == "zero"){ return D3D11_BLEND_ZERO; }
+	if (_type == "one"){ return D3D11_BLEND_ONE; }
+	if (_type == "src_color"){ return D3D11_BLEND_SRC_COLOR; }
+	if (_type == "inv_src_color"){ return D3D11_BLEND_SRC_ALPHA; }
+	if (_type == "src_alpha"){ return D3D11_BLEND_INV_SRC_COLOR; }
+	if (_type == "inv_src_alpha"){ return D3D11_BLEND_INV_SRC_ALPHA; }
+	if (_type == "dest_alpha"){ return D3D11_BLEND_DEST_ALPHA; }
+	if (_type == "inv_dest_alpha"){ return D3D11_BLEND_INV_DEST_ALPHA; }
+	if (_type == "dest_color"){ return D3D11_BLEND_DEST_COLOR; }
+	if (_type == "inv_dest_color"){ return D3D11_BLEND_INV_DEST_COLOR; }
+	if (_type == "src_alpha_sat"){ return D3D11_BLEND_SRC_ALPHA_SAT; }
+	if (_type == "blend_factor"){ return D3D11_BLEND_BLEND_FACTOR; }
+	if (_type == "inv_blend_factor"){ return D3D11_BLEND_INV_BLEND_FACTOR; }
+	if (_type == "src1_color"){ return D3D11_BLEND_SRC1_COLOR; }
+	if (_type == "inv_src1_color"){ return D3D11_BLEND_INV_SRC1_COLOR; }
+	if (_type == "src1_alpha"){ return D3D11_BLEND_SRC1_ALPHA; }
+	if (_type == "inv_src1_alpha"){ return D3D11_BLEND_INV_SRC1_ALPHA; }
+}
+
+
+CBlendOp::CBlendOp(const CXMLTreeNode &TreeNode, const std::string _name):
+	CNamed(_name),
+	m_BlendOp(D3D11_BLEND_OP_ADD)
+{
+	const char *charName = _name.c_str();
+	m_BlendOp = CheckBlendOpType(TreeNode.GetPszProperty(charName));
+}
+
+void CBlendOp::Destroy()
+{
+	// CHECKED_RELEASE(m_BlendOp); CHECKED_DESTROY(m_BlendOp); OJOCUIDAO
+}
+
+int CBlendOp::CheckBlendOpType(const std::string _type)
+{
+	if (_type == "add"){ return D3D11_BLEND_OP_ADD; }
+	if (_type == "substract"){ return D3D11_BLEND_OP_SUBTRACT; }
+	if (_type == "rev_substract"){ return D3D11_BLEND_OP_REV_SUBTRACT; }
+	if (_type == "min"){ return D3D11_BLEND_OP_MIN; }
+	if (_type == "max"){ return D3D11_BLEND_OP_MAX; }
 }
