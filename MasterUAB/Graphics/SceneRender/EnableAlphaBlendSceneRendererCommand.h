d19382f2b6eeb68cc10f53c77597c203edee646b
Ynozzar <aeyann@gmail.com>
2016-02-26 13:03:48 +0100

    Creadas clases de blend y blendop.

--- MasterUAB/Graphics/SceneRender/EnableAlphaBlendSceneRendererCommand.h
+++ MasterUAB/Graphics/SceneRender/EnableAlphaBlendSceneRendererCommand.h
@@ -6,22 +6,43 @@ class CEnableAlphaBlendSceneRendererCommand :
 	public CSceneRendererCommand
 {
 private:
-	std::vector<D3D11_BLEND> m_Blend;
-
-	srcBlend;
-	destBlend;
-	opBlend;
-
-	AlphasrcBlend;
-	AlphadestBlend;
-	AlphaopBlend;
-
-	D3D11_BLEND CheckBlendType(std::string _type);
+	CBlend m_SrcBlend;
+	CBlend m_DestBlend;
+	CBlend m_SrcAlphaBlend;					//C2146 falta ';' delante del identificador  --> C4430 falta especificador de tipo; se presupone int. Nota: C++ no admite default-int
+	CBlend m_DestAlphaBlend;
+	CBlendOp m_OpBlend;
+	CBlendOp m_OpAlphaBlend;
 public:
 	CEnableAlphaBlendSceneRendererCommand(CXMLTreeNode &TreeNode);
 	virtual ~CEnableAlphaBlendSceneRendererCommand();
 
 	virtual void Execute(CRenderManager &RenderManager);
+};
+
+class CBlend : public CNamed
+{
+private:
+	int m_Blend;
+
+	int CheckBlendType(const std::string _type);
+	void Destroy();
+public:
+	CBlend():CNamed(""){} // Si no hago esto, da error de que no tiene constructor predefinido.
+	CBlend(const CXMLTreeNode &TreeNode, const std::string _name);
+	virtual ~CBlend() { Destroy(); }
+};
+
+class CBlendOp : public CNamed
+{
+private:
+	int m_BlendOp;
+
+	int CheckBlendOpType(const std::string _type);
+	void Destroy();
+public:
+	CBlendOp() :CNamed(""){}
+	CBlendOp(const CXMLTreeNode &TreeNode, const std::string _name);
+	virtual ~CBlendOp() { Destroy(); }
 };
 
 #endif //ENABLE_ALPHA_BLEND_SCENE_RENDERER_COMMAND_H
