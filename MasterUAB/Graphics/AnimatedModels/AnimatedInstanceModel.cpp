ebfbb36c6338f1d9f6bea5da7edff14cbc486a3d
Laura <nah_one@hotmail.com>
2016-03-04 21:29:13 +0100

    implementado controller player

--- MasterUAB/Graphics/AnimatedModels/AnimatedInstanceModel.cpp
+++ MasterUAB/Graphics/AnimatedModels/AnimatedInstanceModel.cpp
@@ -90,6 +90,7 @@ void CAnimatedInstanceModel::Initialize(CAnimatedCoreModel *AnimatedCoreModel)
 
 void CAnimatedInstanceModel::Render(CRenderManager *_RenderManager)
 {
+	CRenderableObject::Render(_RenderManager);
 	CEffectManager::m_SceneParameters.m_World=GetTransform();
 	for(int l_HardwareMeshId=0; l_HardwareMeshId<m_CalHardwareModel->getHardwareMeshCount(); ++l_HardwareMeshId)
 	{
@@ -121,6 +122,7 @@ void CAnimatedInstanceModel::Render(CRenderManager *_RenderManager)
 }
 void CAnimatedInstanceModel::Update(float ElapsedTime)
 {
+	CRenderableObject::Update(ElapsedTime);
 	m_CalModel->update(ElapsedTime);
 }
 void CAnimatedInstanceModel::Destroy()
@@ -158,4 +160,9 @@ bool CAnimatedInstanceModel::IsCycleAnimationActive(int Id) const
 bool CAnimatedInstanceModel::IsActionAnimationActive(int Id) const
 {
 	return m_ActualActionAnimation==Id;
+}
+
+void CAnimatedInstanceModel::RenderDebug(CRenderManager *RenderManager)
+{
+	CRenderableObject::RenderDebug(RenderManager);
 }
