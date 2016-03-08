520dbbe20b7238ca814a32b6ee6ecab78e53d544
jonathanhr <jonathan.hortelano@gmail.com>
2016-03-08 21:27:49 +0100

    implementado particulas

---
+++ MasterUAB/Graphics/Particles/ParticleManager.cpp
@@ -0,0 +1,35 @@
+#include "ParticleManager.h"
+#include "XML\XMLTreeNode.h"
+
+CParticleManager::CParticleManager(void){}
+
+CParticleManager::~CParticleManager(void)
+{
+	Destroy();
+}
+
+
+void CParticleManager::Load(const std::string &Filename)
+{
+	CXMLTreeNode l_File;
+	m_Filename = Filename;
+
+	if (l_File.LoadFile(m_Filename.c_str()))
+	{
+		CXMLTreeNode l_Meshes = l_File["particle_systems"];
+		for (int i = 0; i < l_Meshes.GetNumChildren(); ++i)
+		{
+			CParticleSystemType *l_ParticleSystemType = new CParticleSystemType(l_Meshes(i));
+			if (!AddResource(l_ParticleSystemType->GetName(), l_ParticleSystemType, "CParticleManager"))
+			{
+				CHECKED_DELETE(l_ParticleSystemType);
+			}
+		}
+	}
+}
+
+void CParticleManager::Reload()
+{
+	Destroy();
+	Load(m_Filename);
+}
---
+++ MasterUAB/Graphics/Particles/ParticleSystemType.h
@@ -0,0 +1,56 @@
+#ifndef PARTICLE_SYSTEM_TYPE_H
+#define PARTICLE_SYSTEM_TYPE_H
+
+#include "Utils.h"
+#include "Math\Matrix44.h"
+#include "Math\Color.h"
+
+#include <string>
+
+class CMaterial;
+class CXMLTreeNode;
+
+class CParticleSystemType
+{
+private:
+	CMaterial *m_Material;
+	int m_NumFrames;	
+	float m_TimePerFrame;
+	bool m_LoopFrames;
+	bool m_EmitAbsolute;
+	float m_StartingDirectionAngle, m_StartingAccelerationAngle;
+	Vect2f m_Size;
+	Vect2f m_EmitRate, m_AwakeTime, m_SleepTime, m_Life;
+	Vect2f m_StartingAngle, m_StartingAngularSpeed, m_AngularAcceleration;
+	Vect3f m_StartingSpeed1, m_StartingSpeed2;
+	Vect3f m_StartingAcceleration1, m_StartingAcceleration2;
+	CColor m_Color1, m_Color2;
+
+public:
+	CParticleSystemType(CXMLTreeNode &TreeNode);
+	virtual ~CParticleSystemType(void);
+
+	UAB_BUILD_GET_SET(CMaterial *, Material);
+	UAB_GET_PROPERTY(int, NumFrames);
+	UAB_GET_PROPERTY(float, TimePerFrame);
+	UAB_GET_PROPERTY(bool, LoopFrames);
+	UAB_GET_PROPERTY(bool, EmitAbsolute);
+	UAB_GET_PROPERTY(float, StartingDirectionAngle);
+	UAB_GET_PROPERTY(float, StartingAccelerationAngle);
+	UAB_GET_PROPERTY(Vect2f, Size);
+	UAB_GET_PROPERTY(Vect2f, EmitRate);
+	UAB_GET_PROPERTY(Vect2f, AwakeTime);
+	UAB_GET_PROPERTY(Vect2f, SleepTime);
+	UAB_GET_PROPERTY(Vect2f, Life);
+	UAB_GET_PROPERTY(Vect2f, StartingAngle);
+	UAB_GET_PROPERTY(Vect2f, StartingAngularSpeed);
+	UAB_GET_PROPERTY(Vect2f, AngularAcceleration);
+	UAB_GET_PROPERTY(Vect3f, StartingSpeed1);
+	UAB_GET_PROPERTY(Vect3f, StartingSpeed2);
+	UAB_GET_PROPERTY(Vect3f, StartingAcceleration1);
+	UAB_GET_PROPERTY(Vect3f, StartingAcceleration2);
+	UAB_GET_PROPERTY(CColor, Color1);
+	UAB_GET_PROPERTY(CColor, Color1);	
+};
+
+#endif //PARTICLE_SYSTEM_TYPE_H
