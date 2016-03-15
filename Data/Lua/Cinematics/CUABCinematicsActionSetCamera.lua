da7abae673467d198b22d3d44536335b23497e71
Paula <nah_one@hotmail.com>
2016-03-15 16:45:56 +0100

    Van las cinematicas con Lua

--- Data/Lua/Cinematics/CUABCinematicsActionSetCamera.lua
+++ Data/Lua/Cinematics/CUABCinematicsActionSetCamera.lua
@@ -1,12 +1,13 @@
 class 'CUABCinematicsActionSetCamera' (CUABCinematicsAction)
 	function CUABCinematicsActionSetCamera:__init(XMLTreeNode)
 		CUABCinematicsAction.__init(self, XMLTreeNode)
-		self.m_CameraName=XMLTreeNode:GetPszProperty("name", "")
+		self.m_CameraName=XMLTreeNode:get_psz_property("name", "", false)
 	end
 	
 	function CUABCinematicsActionSetCamera:Execute()
-		local l_Engine=KGEngine()
-		local l_CameraManager=l_Engine:GetCameraManager()
-		l_CameraManager:SetControlCameraController(l_CameraManager:GetResource(self.m_CameraName))
+		local l_Engine = CUABEngine.get_instance()
+		local l_CameraManager=l_Engine:get_camera_controller_manager()
+		utils_log(""..self.m_CameraName)
+		l_CameraManager:choose_main_camera(self.m_CameraName)
 	end
 --end
