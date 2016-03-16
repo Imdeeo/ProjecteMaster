3f87c494d0c7e75b75b1daf65bf164ad48432d7c
Imdeeo <joanillo2002@gmail.com>
2016-03-16 19:17:12 +0100

    Init Lua Mien!

--- Data/Lua/init.lua
+++ Data/Lua/init.lua
@@ -1,7 +1,13 @@
 dofile("Data\\Lua\\Player\\Player.lua")
+dofile("Data\\Lua\\Cinematics\\CUABCinematicsActionManager.lua")
 dofile("Data\\Lua\\componentsEnemy.lua")
 dofile("Data\\Lua\\antweakbar.lua")
 function mainLua()
+	InitPlayer()
 	InitAntweakBar()
-	--InitEnemyMove()
+	InitEnemyMove()
+end
+
+function luaUpdate(_ElapsedTime)
+
 end
