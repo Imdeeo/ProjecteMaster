340d0335beb072d7a0ec7aa7418e23753687c2e3
Roser <nah_one@hotmail.com>
2016-03-27 10:36:21 +0200

    Un poquito mas :3

--- MasterUAB/GUI/GUIManager.cpp
+++ MasterUAB/GUI/GUIManager.cpp
@@ -1,9 +1,11 @@
 #include "GUIManager.h"
 #include "Materials\Material.h"
-#include "SpriteMap.h"
-#include "Sprite.h"
+#include "RenderableObjects\RenderableVertexs.h"
 #include "Buton.h"
 #include "Slider.h"
+#include "RenderManager\RenderManager.h"
+#include "RenderableObjects\VertexTypes.h"
+#include "Materials\Material.h"
 
 void CGUIManager::SetActive(const std::string& id)
 {
@@ -28,5 +30,33 @@ void CGUIManager::SetNotHot(const std::string& id)
 	if (m_HotItem == id)
 	{
 		m_HotItem = "";
+	}
+}
+
+void CGUIManager::Render(CRenderManager *RenderManager)
+{
+	MV_POSITION4_COLOR_TEXTURE_VERTEX currentBufferData[s_MaxVerticesPerCall];
+	int currentVertex = 0;
+	SpriteMapInfo *currentSpriteMap = nullptr;
+	for (int i = 0; i < commandsExecutionOrder.size(); ++i)
+	{
+		GUICommand &command = m_Commands[commandsExecutionOrder[i]];
+		assert(command.x1 <= command.x2);
+		assert(command.y2 <= command.y2);
+
+		SpriteInfo *commandSprite = command.sprite;
+		SpriteMapInfo *commandSpriteMap = commandSprite->SpriteMap;
+
+		if (currentSpriteMap != commandSpriteMap || currentVertex == s_MaxverticesPerCall)
+		{
+			if (currentVertex > 0)
+			{
+				//TODO log a warning if we get here by "currentVertex == s_MaxVerticesPerCall"
+				//TODO draw all c urrent vertex in the currentBuffer
+			}
+			currentVertex = 0;
+			currentSpriteMap = commandSpriteMap;
+		}
+	
 	}
 }
--- MasterUAB/GUI/GUIManager.h
+++ MasterUAB/GUI/GUIManager.h
@@ -7,30 +7,54 @@
 #include <vector>
 #include <map>
 
-class CSpriteMap;
-class CSprite;
 class CButon;
 class CSlider;
 class CMaterial;
+class CRenderableVertex;
+class CColor;
+class CRenderManager;
 
+struct SpriteMapInfo
+{
+	int MaterialIndex;
+	int w, h;
+};
+
+struct SpriteInfo
+{
+	SpriteMapInfo *SpriteMap;
+	float u1, u2, v1, v2;
+};
+
+struct GUICommand
+{
+	SpriteInfo *sprite;
+	int x1, y1, x2, y2; //top left is 0,0
+	float u1, v1, u2, v2;
+	Vect4f color;
+};
 
 class CGUIManager 
 {
 private:
 	std::string m_ActiveItem;
 	std::string m_HotItem;
-	std::vector<CMaterial> m_Material;
-	std::vector<ID3D11Buffer> m_VertexBuffer;
-	std::map<std::string, CSpriteMap> m_SpriteMaps; 
-	std::map<std::string, CSprite> m_Sprites;
+	std::vector<CRenderableVertex> m_VertexBuffers;
+	std::vector<CMaterial> m_Materials;
+	std::vector<GUICommand> m_Commands;	
+	std::map<std::string, SpriteMapInfo> m_SpriteMaps; 
+	std::map<std::string, SpriteInfo> m_Sprites;
 	std::map<std::string, CButon> m_Buttons;
 	std::map<std::string, CSlider> m_Sliders;
+	
 
 public:
+
 	void SetActive(const std::string& id);
 	void SetNotActive(const std::string& id);
 	void SetHot(const std::string& id);
 	void SetNotHot(const std::string& id);
+	void Render(CRenderManager *RenderManager);
 };
 
 #endif //H_GUI_MANAGER_H
