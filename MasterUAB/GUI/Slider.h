1c87f0ed46e09de2e803711a7f8c23c371d71347
Paula <nah_one@hotmail.com>
2016-03-29 12:04:14 +0200

    more

--- MasterUAB/GUI/Slider.h
+++ MasterUAB/GUI/Slider.h
@@ -2,16 +2,16 @@
 #define H_SLIDER_H
 
 #include "Utils.h"
-#include "Sprite.h"
+#include "GUIManager.h"
 
 class CSlider
 {
 private:
 public:
-	UAB_BUILD_GET_SET(CSprite*, Base)
-	UAB_BUILD_GET_SET(CSprite*, Top)
-	UAB_BUILD_GET_SET(CSprite*, Handle)
-	UAB_BUILD_GET_SET(CSprite*, PressedHandle)
+	UAB_BUILD_GET_SET(SpriteInfo*, Base)
+	UAB_BUILD_GET_SET(SpriteInfo*, Top)
+	UAB_BUILD_GET_SET(SpriteInfo*, Handle)
+	UAB_BUILD_GET_SET(SpriteInfo*, PressedHandle)
 };
 
 #endif //H_SLIDER_H
