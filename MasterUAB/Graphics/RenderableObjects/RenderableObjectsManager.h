87870956ae89cd6224892e165e579e52837a21c8
jonathanhr <jonathan.hortelano@gmail.com>
2016-03-29 13:57:13 +0200

    arreglado error

--- MasterUAB/Graphics/RenderableObjects/RenderableObjectsManager.h
+++ MasterUAB/Graphics/RenderableObjects/RenderableObjectsManager.h
@@ -29,7 +29,6 @@ public:
 		const std::string &InstanceModelName, const Vect3f &Position, bool _Update = false);
 
 	CRenderableObjectsManager & CRenderableObjectsManager::operator=(CRenderableObjectsManager&);
-		const std::string &InstanceModelName, const Vect3f &Position);
 	CRenderableObject * AddParticleSystemInstance(CXMLTreeNode &TreeNode);
 };
 
