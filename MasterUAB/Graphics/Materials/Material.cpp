6bd6cd426bf434a936223ac58d16e68da759f7e0
Uri <nah_one@hotmail.com>
2016-05-05 20:38:28 +0200

    Mini arreglo del Merge

--- MasterUAB/Graphics/Materials/Material.cpp
+++ MasterUAB/Graphics/Materials/Material.cpp
@@ -30,7 +30,7 @@ CMaterial::CMaterial(const CXMLTreeNode &TreeNode) : CNamed(TreeNode), m_Current
 		if (l_Child.GetName() == std::string("texture"))
 		{
 			CTexture* l_texture = CUABEngine::GetInstance()->GetTextureManager()->GetTexture(l_Child.GetPszProperty("filename"));
-			std::string l_TextureType = l_Child.GetPszProperty("type");
+			std::string l_TextureType = l_Child.GetPszProperty("type", "diffuse", true);
 			l_texture->SetType(l_TextureType);
 
 			if (l_TextureType == "lightmap")
