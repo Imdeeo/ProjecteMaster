cbbaa89fc603c2aebaa8140499f936c71dbafbbb
Robb <trincasidra@users.noreply.github.com>
2016-05-08 19:52:14 +0200

    Corregido bug en la state_machine. Implementada PlayerStateMachine, con los estados funcionales Idle, Moving, y Jumping.

--- Data/Lua/Player/PlayerStateMachine.lua
+++ Data/Lua/Player/PlayerStateMachine.lua
@@ -1,22 +1,30 @@
 dofile("Data\\Lua\\Utils\\state_machine.lua")
 dofile("Data\\Lua\\Player\\PlayerStateIdle.lua")
 dofile("Data\\Lua\\Player\\PlayerStateMoving.lua")
+dofile("Data\\Lua\\Player\\PlayerStateJumping.lua")
 dofile("Data\\Lua\\Player\\PlayerStateInteracting.lua")
 dofile("Data\\Lua\\Player\\PlayerStateDead.lua")
 
 PlayerStateMachine = StateMachine.create()
 
 function setPlayerStateMachine()
-utils_log("set player state machine")
+	utils_log("set player state machine")
 	IdleState = State.create(IdleUpdate)
 	IdleState:set_do_first_function(IdleFirst)
 	IdleState:set_do_end_function(IdleEnd)
 	IdleState:add_condition(IdleToMovingCondition, "Moving")
+	IdleState:add_condition(IdleToJumpingCondition, "Jumping")
 	
 	MovingState = State.create(MovingUpdate)
 	MovingState:set_do_first_function(MovingFirst)
 	MovingState:set_do_end_function(MovingEnd)
 	MovingState:add_condition(MovingToIdleCondition, "Idle")
+	MovingState:add_condition(MovingToJumpingCondition, "Jumping")
+	
+	JumpingState = State.create(JumpingUpdate)
+	JumpingState:set_do_first_function(JumpingFirst)
+	JumpingState:set_do_end_function(JumpingEnd)
+	JumpingState:add_condition(JumpingToIdleCondition, "Idle")
 	
 	InteractingState = State.create(InteractingUpdate)
 	InteractingState:set_do_first_function(InteractingFirst)
@@ -28,6 +36,7 @@ utils_log("set player state machine")
 	
 	PlayerStateMachine:add_state("Idle", IdleState)
 	PlayerStateMachine:add_state("Moving", MovingState)
+	PlayerStateMachine:add_state("Jumping", JumpingState)
 	PlayerStateMachine:add_state("Interacting", InteractingState)
 	PlayerStateMachine:add_state("Dead", DeadState)
 end
--- Data/Lua/Player/PlayerStateMoving.lua
+++ Data/Lua/Player/PlayerStateMoving.lua
@@ -1,44 +1,26 @@
 function MovingFirst(args)
-	
+	utils_log("MovingFirst")
 end
 
 function MovingUpdate(args, _ElapsedTime)
 	local l_Owner = args["owner"]
-	local l_Player = CCharacterManager.get_instance():get_resource("player")
-	local l_InputManager = CInputManager.get_input_manager()
-	local l_PhysXManager = CUABEngine.get_instance():get_physX_manager()
-	local l_ForwardMovement = l_InputManager:get_axis("MOVE_FWD")
-	local l_StrafeMovement = l_InputManager:get_axis("STRAFE")
-	
-	if(l_InputManager:is_action_active("JUMP")==true and g_IsJumping==false and g_Velocity.y == 0) then
-		g_Velocity.y = 4.9
-		g_IsJumping =  true
-		g_IsAscending = true
-	end
-	
-	if(g_IsAscending==true and g_Velocity.y<0) then
-		g_IsAscending = false
-	end
-	
-	if( g_IsAscending==false and g_Velocity.y == 0)then
-		g_IsJumping = false
-	end
-	
+	local l_ForwardMovement = g_InputManager:get_axis("MOVE_FWD")
+	local l_StrafeMovement = g_InputManager:get_axis("STRAFE")
 	
 	--// Move player forward and laterally
-	local l_CameraDirection = l_Player:get_camera_controller():get_forward():get_normalized(1)
+	local l_CameraDirection = g_Player:get_camera_controller():get_forward():get_normalized(1)
 	local l_CameraXZDirection = Vect2f(l_ForwardMovement*l_CameraDirection.x,l_ForwardMovement*l_CameraDirection.z)
 	local l_CameraXZDirectionOrt = Vect2f(-l_CameraDirection.z*l_StrafeMovement,l_StrafeMovement*l_CameraDirection.x)
 	local l_NewPlayerDirection = Vect2f(l_CameraXZDirection.x + l_CameraXZDirectionOrt.x,l_CameraXZDirection.y + l_CameraXZDirectionOrt.y)
 	local l_PlayerDisplacement = Vect3f(l_NewPlayerDirection.x*g_Speed,g_Velocity.y+g_Gravity * _ElapsedTime,l_NewPlayerDirection.y*g_Speed)
 	
 	--// Move the character controller
-	local l_PreviousControllerPosition = l_PhysXManager:get_character_controler_pos("player")
+	local l_PreviousControllerPosition = g_PhysXManager:get_character_controler_pos("player")
 	l_PreviousControllerPosition.y = l_PreviousControllerPosition.y - 0.9
-	l_PhysXManager:character_controller_move("player", l_PlayerDisplacement, _ElapsedTime)
+	g_PhysXManager:character_controller_move("player", l_PlayerDisplacement, _ElapsedTime)
 	
 	--// Assign to the character the controller's position
-	local l_NewControllerPosition = l_PhysXManager:get_character_controler_pos("player")
+	local l_NewControllerPosition = g_PhysXManager:get_character_controler_pos("player")
 	l_NewControllerPosition.y = l_NewControllerPosition.y - 0.9
 	l_Owner:set_position(l_NewControllerPosition)
 	
@@ -49,7 +31,7 @@ function MovingUpdate(args, _ElapsedTime)
 	--// Rotate player to match camera
 	l_RotationXZ = Quatf()
 	l_RotationY = Quatf()
-	l_Rotation = l_Player:get_camera_controller():get_rotation()
+	l_Rotation = g_Player:get_camera_controller():get_rotation()
 	l_Rotation:decouple_y(l_RotationXZ, l_RotationY)
 	l_Owner:set_rotation(l_RotationY)
 	
@@ -61,7 +43,7 @@ function MovingUpdate(args, _ElapsedTime)
 	local l_DisplacementModule = math.sqrt(l_X + l_Y + l_Z)
 	
 	--// Animate player
-	l_Owner:clear_cycle(l_Owner:get_actual_cycle_animation(),0.1);
+	l_Owner:clear_cycle(l_Owner:get_actual_cycle_animation(),0.1)
 	if l_DisplacementModule == 0 then		
 		l_Owner:blend_cycle(1,1.0,0.1);
 	else
@@ -70,12 +52,13 @@ function MovingUpdate(args, _ElapsedTime)
 end
 
 function MovingEnd(args)
-	
+	utils_log("MovingEnd")
 end
 
 function MovingToIdleCondition()
-	local l_physXManager = CUABEngine.get_instance():get_physX_manager()	
-		local l_InputManager = CInputManager.get_input_manager()
-	return not (l_InputManager:is_action_active("MOVE_FWD") or l_InputManager:is_action_active("MOVE_BACK") or l_InputManager:is_action_active("STRAFE_LEFT") or l_InputManager:is_action_active("STRAFE_RIGHT"))
+	return not (g_InputManager:is_action_active("MOVE_FWD") or g_InputManager:is_action_active("MOVE_BACK") or g_InputManager:is_action_active("STRAFE_LEFT") or g_InputManager:is_action_active("STRAFE_RIGHT"))
+end
 
+function MovingToJumpingCondition()
+	return g_InputManager:is_action_active("JUMP")
 end
