a8938e04c6d55c6d6008f8696027d85a2441b5a8
jonathanhr <jonathan.hortelano@gmail.com>
2016-05-22 17:49:33 +0200

    implementado enemigo automata en lua

--- Data/Lua/Enemies/AutomatonEnemy/AutomatonEnemy.lua
+++ Data/Lua/Enemies/AutomatonEnemy/AutomatonEnemy.lua
@@ -1,8 +1,9 @@
-dofile("Data\\Lua\\Enemies\\Automaton\\AutomatonStateMachine.lua")
+dofile("Data\\Lua\\Enemies\\AutomatonEnemy\\AutomatonStateMachine.lua")
 
 function FnOnCreateAutomaton (_owner)
-	g_Player.m_PhysXManager:register_material("controllerMaterial", 0.5, 0.5, 0.1)
-	g_Player.m_PhysXManager:create_character_controller(g_Player.m_Name, 1.2, 0.3, 0.5, _owner:get_position(),"controllerMaterial", "Enemy")
+	l_Enemy = m_CharacterManager.m_Enemies[1]
+	l_Enemy.m_PhysXManager:register_material("controllerMaterial", 0.5, 0.5, 0.1)
+	l_Enemy.m_PhysXManager:create_character_controller(l_Enemy.m_Name, 1.2, 0.3, 0.5, _owner:get_position(),"controllerMaterial", "Enemy")
 end
 
 function FnOnDestroyAutomaton ()
@@ -12,7 +13,7 @@ end
 function FnOnUpdateAutomaton (_owner, _ElapsedTime)
 	local args = {}
 	args["owner"] = _owner
-	PlayerStateMachine:update(args, _ElapsedTime)
+	AutomatonStateMachine:update(args, _ElapsedTime)
 end
 
 function FnOnRenderAutomaton (_owner, _rm)
@@ -20,5 +21,5 @@ function FnOnRenderAutomaton (_owner, _rm)
 end
 
 function FnOnDebugRenderAutomaton (_owner, _rm)
-	--g_PhysXManager:render("player", _rm)
+	--g_PhysXManager:render("enemy", _rm)
 end
