8ff4cbea95ff6c8f14342c1d44ca65e46daab128
jonathanhr <jonathan.hortelano@gmail.com>
2016-05-24 13:28:58 +0200

    mas automata state machine

--- Data/Lua/Enemies/AutomatonEnemy/AutomatonStateOff.lua
+++ Data/Lua/Enemies/AutomatonEnemy/AutomatonStateOff.lua
@@ -4,13 +4,12 @@ end
 
 function OffUpdateAutomaton(args, _ElapsedTime)
 	local l_Owner = args["owner"]
+	local l_Enemy = m_CharacterManager.m_Enemics[1]
+	
 	l_Distance = g_Player.m_RenderableObject:get_position():distance(l_Owner:get_position())
 	
 	if l_Distance < 10.0 then
-		utils_log("se mueve")
-		l_Owner:blend_cycle(0,1.0,0.1)
-	else
-		utils_log("muevete cerdo")
+		l_Enemy.m_Activate = true
 	end
 end
 
@@ -18,6 +17,7 @@ function OffEndAutomaton(args)
 	utils_log("OffEnd")
 end
 
-function OffToIdleConditionAutomaton()
-	--return (g_Player.m_InputManager:is_action_active("MOVE_FWD") or g_Player.m_InputManager:is_action_active("MOVE_BACK") or g_Player.m_InputManager:is_action_active("STRAFE_LEFT") or g_Player.m_InputManager:is_action_active("STRAFE_RIGHT"))
+function OffToIdleConditionAutomaton()	
+	local l_Enemy = m_CharacterManager.m_Enemics[1]
+	return l_Enemy.m_Activate
 end
