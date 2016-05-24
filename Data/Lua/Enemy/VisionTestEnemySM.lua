f7e7bb569258346c354c23ff42c57afbf9457f5f
Juho Vikman <jvikman@users.noreply.github.com>
2016-05-24 12:54:26 +0200

    CEnemyView in EnemyUtils

--- Data/Lua/Enemy/VisionTestEnemySM.lua
+++ Data/Lua/Enemy/VisionTestEnemySM.lua
@@ -1,40 +1,49 @@
 dofile("Data\\Lua\\Utils\\state_machine.lua")
+dofile("Data\\Lua\\Utils\\EnemyUtils.lua")
 
-class "VisionTestEnemySM"
-  function VisionTestEnemySM:__init(owner)
+class "CVisionTestEnemySM"
+  function CVisionTestEnemySM:__init(_owner)
     utils_log("VisionTestEnemySM:__init()")
-    self.owner = owner
+    self.owner = _owner
     self.m_StateMachine = StateMachine.create()
-    local scope_state = State.create(function (args, _ElapsedTime)
-      self:scope_update(args, _ElapsedTime)
-    end)
-    scope_state:add_condition(function ()
-      return self:player_in_sight()
-    end, "watch")
-    scope_state:set_do_first_function(function(owner)
-      self:scopeFirst(owner)
-    end)
-    --scope_state:set_do_end_function()
+    self.m_Vision = CEnemyVision(_owner)
+    self.m_LatestBlockingObjectName = nil
 
-    local watch_state = State.create(function (args, _ElapsedTime)
-      self:watch_update(args)
-    end)
-    watch_state:add_condition(function ()
-      return not self:player_in_sight()
-    end, "scope")
+    local scope_state = State.create(
+      function (args, _ElapsedTime)
+        self:scope_update(args, _ElapsedTime)
+      end
+    )
+    scope_state:add_condition(function ()
+      return self.m_Vision:PlayerVisible()
+    end, "watch")
+    scope_state:set_do_first_function(scopeFirst)
+
+    local watch_state = State.create(
+      function (args, _ElapsedTime)
+        self:watch_update(args)
+      end
+    )
+    watch_state:add_condition(
+      function ()
+        return not self.m_Vision:PlayerVisible()
+      end,
+      "scope"
+    )
     watch_state:set_do_first_function(watchFirst)
-    --watch_state:set_do_end_function()
+    watch_state:set_do_end_function(
+      function ()
+        self.m_LatestBlockingObjectName = nil
+      end
+    )
 
     self.m_StateMachine:add_state("scope", scope_state)
     self.m_StateMachine:add_state("watch", watch_state)
-    self.m_MaxDistance = 50.0
-    self.m_MaxAngle = math.pi * 0.25
     self.m_StateMachine:start()
-    self.m_LatestBlockingObjectName = nil
-    self.m_BlockingObjectName = nil
   end
 
-  function VisionTestEnemySM:scope_update(args, _ElapsedTime)
+  function CVisionTestEnemySM:scope_update(args, _ElapsedTime)
+    -- rotate constantly
     local owner = args["owner"]
     local d_rot = Quatf()
     d_rot:quat_from_yaw_pitch_roll(1, 0.0, 0.0)
@@ -42,13 +51,15 @@ class "VisionTestEnemySM"
     local target_quat = d_rot * rot
     local actual_rot = rot:slerp(target_quat, _ElapsedTime)
     owner:set_rotation(actual_rot)
-    if self.m_BlockingObjectName ~= self.m_LatestBlockingObjectName then
-      utils_log("Object blocking the view: " .. self.m_BlockingObjectName)
-      self.m_LatestBlockingObjectName = self.m_BlockingObjectName
+
+    if self.m_Vision.m_BlockingObjectName ~= self.m_LatestBlockingObjectName then
+      utils_log("Object blocking the view: " .. self.m_Vision.m_BlockingObjectName)
+      self.m_LatestBlockingObjectName = self.m_Vision.m_BlockingObjectName
     end
   end
 
-  function VisionTestEnemySM:watch_update(args, _ElapsedTime)
+  function CVisionTestEnemySM:watch_update(args, _ElapsedTime)
+    -- turn to face player
     local owner = args["owner"]
     local l_physXManager = CUABEngine.get_instance():get_physX_manager()
     local playerPos = l_physXManager:get_character_controler_pos("player")
@@ -58,25 +69,19 @@ class "VisionTestEnemySM"
     direction.y = 0.0
     local forward = owner:get_rotation():get_forward_vector()
     forward.y = 0.0
-    --[[if direction:length() ~= 0.0 then
-    end
-    if forward:length() ~= 0.0 then
-    end]]
     if direction:length() == 0.0 or forward:length() == 0.0 then
       utils_log("Zero length vector!")
       return
     end
     direction:normalize(1.0)
     forward:normalize(1.0)
+
     local dot = direction * forward
     if 1.0 - dot < 0.01 then
       return
     end
     local angle_to_turn = math.acos(dot)
     local cross = direction ^ forward
-    if angle_to_turn < 0.01 then
-      return
-    end
     if cross.y < 0.0 then
       angle_to_turn = -angle_to_turn
     end
@@ -86,37 +91,7 @@ class "VisionTestEnemySM"
     owner:set_rotation(target_quat)
   end
 
-  function VisionTestEnemySM:player_in_sight()
-    local l_physXManager = CUABEngine.get_instance():get_physX_manager()
-    local playerPos = l_physXManager:get_character_controler_pos("player")
-    local ownerPos = self.owner:get_position()
-    local dist = playerPos:distance(ownerPos)
-    if dist > self.m_MaxDistance then
-      return false
-    end
-
-    local l_PlayerDirection = playerPos - ownerPos
-    l_PlayerDirection:normalize(1.0)
-    local l_Forward = self.owner:get_rotation():get_forward_vector()
-    local dot = l_Forward * l_PlayerDirection
-    if dot < math.cos(self.m_MaxAngle) then
-      return false
-    end
-
-    local l_RaycastHeight = Vect3f(0.0, 1.0, 0.0)
-    local l_RaycastData = RaycastData()
-    local hit = l_physXManager:raycast(ownerPos+l_RaycastHeight, playerPos+l_RaycastHeight, 15, l_RaycastData)
-    if hit and l_RaycastData.actor_name ~= "player" then
-      self.m_BlockingObjectName = l_RaycastData.actor_name
-      return false
-    end
-
-    self.m_BlockingObjectName = nil
-    self.m_LatestBlockingObjectName = nil
-    return true
-  end
-
-  function VisionTestEnemySM:scopeFirst()
+  function scopeFirst()
     utils_log("VisionTestEnemy: looking around")
   end
 
