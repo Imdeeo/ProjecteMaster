6d2cb7ba0f0d4156705131ec182e44cfb4d46f08
jonathanhr <jonathan.hortelano@gmail.com>
2016-05-25 16:52:01 +0200

    mas automata

--- Data/Lua/Enemies/AutomatonEnemy/AutomatonStateAttack.lua
+++ Data/Lua/Enemies/AutomatonEnemy/AutomatonStateAttack.lua
@@ -1,33 +1,11 @@
 function AttackFirstAutomaton(args)
 	utils_log("AttackFirst")
+	local l_Owner = args["owner"]
+	l_Owner:clear_cycle(0,0.1)
 end
 
 function AttackUpdateAutomaton(args, _ElapsedTime)
-	--[[local l_Owner = args["owner"]
 	
-	--// Calculate the player speed
-	local l_PlayerDisplacement = Vect3f(0, g_Player.m_Velocity.y + g_Player.m_Gravity * _ElapsedTime, 0)
-	
-	--// Move the character controller
-	local l_PreviousControllerPosition = g_Player.m_PhysXManager:get_character_controler_pos("player")
-	l_PreviousControllerPosition.y = l_PreviousControllerPosition.y - 0.9
-	g_Player.m_PhysXManager:character_controller_move("player", l_PlayerDisplacement, _ElapsedTime)
-	
-	--// Assign to the character the controller's position
-	local l_NewControllerPosition = g_Player.m_PhysXManager:get_character_controler_pos("player")
-	l_NewControllerPosition.y = l_NewControllerPosition.y - 0.9
-	l_Owner:set_position(l_NewControllerPosition)
-	
-	--// Save speed in last update so we can create acceleration
-	local l_Displacement = l_NewControllerPosition-l_PreviousControllerPosition
-	g_Player.m_Velocity = l_Displacement/_ElapsedTime
-	
-	--// Rotate player to match camera
-	l_RotationXZ = Quatf()
-	l_RotationY = Quatf()
-	l_Rotation = g_Player.m_CameraController:get_rotation()
-	l_Rotation:decouple_y(l_RotationXZ, l_RotationY)
-	l_Owner:set_rotation(l_RotationY)]]--
 end
 
 function AttackEndAutomaton(args)
--- Data/Lua/Enemies/AutomatonEnemy/AutomatonStateOff.lua
+++ Data/Lua/Enemies/AutomatonEnemy/AutomatonStateOff.lua
@@ -2,20 +2,15 @@ function OffFirstAutomaton(args)
 end
 
 function OffUpdateAutomaton(args, _ElapsedTime)
-	local l_Owner = args["owner"]
-	local l_Enemy = m_CharacterManager.m_Enemics[1]
 	
-	local l_Distance = g_Player.m_RenderableObject:get_position():distance(l_Owner:get_position())
-	
-	if l_Distance < l_Enemy.m_distance_to_activate then
-		l_Enemy.m_Activate = true
-	end
 end
 
 function OffEndAutomaton(args)
 end
 
 function OffToIdleConditionAutomaton()	
-	local l_Enemy = m_CharacterManager.m_Enemics[1]
-	return l_Enemy.m_Activate
+	local l_Enemy = m_CharacterManager.m_Enemics[1]	
+	local l_Distance = g_Player.m_RenderableObject:get_position():distance(l_Enemy.m_RenderableObject:get_position())
+	
+	return l_Distance < l_Enemy.m_distance_to_activate
 end
