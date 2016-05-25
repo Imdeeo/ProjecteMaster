4bf1af97273702155386d9ab0ea88410410f7242
Uri <nah_one@hotmail.com>
2016-05-25 20:11:21 +0200

    more fog

--- Data/Lua/Enemies/FogEnemy/FogStateChase.lua
+++ Data/Lua/Enemies/FogEnemy/FogStateChase.lua
@@ -3,7 +3,10 @@ function FogChaseFirst(args)
 end
 
 function FogChaseUpdate(args, _ElapsedTime)
-	
+	local l_Owner = args["owner"]
+	local l_Enemy = m_CharacterManager.m_Enemics[1]
+	--l_Owner:set_position(l_Owner:get_position() + l_Owner:get_rotation():get_forward_vector() * _ElapsedTime * l_Enemy.m_Speed
+	--local l_NewControllerPosition = g_Player.m_PhysXManager:get_character_controler_pos("player")
 end
 
 function FogChaseEnd(args)
@@ -16,4 +19,8 @@ end
 
 function FogChaseToOffCondition()
 	return m_CharacterManager.m_Enemics[1].m_off == true
+end
+
+function FogChaseToAttackCondition()
+	return false
 end
