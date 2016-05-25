7d7db4afdff21ef8085634da57aa15fca42042c1
Paula <nah_one@hotmail.com>
2016-05-25 11:35:44 +0200

    fagots

--- Data/Lua/Enemies/FogEnemy/FogStateAttack.lua
+++ Data/Lua/Enemies/FogEnemy/FogStateAttack.lua
@@ -3,19 +3,9 @@ function FogAttackFirst(args)
 end
 
 function FogAttackUpdate(args, _ElapsedTime)
-	color = CColor(1,1,1,1)
-	coord = Vect2f(500,300)
-	gui_manager:do_text("fontTest", "MUERE", coord, CGUIManager.mid_center, color)
+	
 end
 
 function FogAttackEnd(args)
 	utils_log("AttackEnd")
-end
-
-function FogAttackToChaseCondition()
-	return false
-end
-
-function FogAttackToOffCondition()
-	return m_CharacterManager.m_Enemics[1].m_off == true
 end
--- Data/Lua/Enemies/FogEnemy/FogStateChase.lua
+++ Data/Lua/Enemies/FogEnemy/FogStateChase.lua
@@ -1,26 +1,51 @@
 function FogChaseFirst(args)
-	utils_log("Chase First")
+	utils_log("start chase")
+	--l_Owner:blend_cycle(0,1.0,0.1)	
 end
 
 function FogChaseUpdate(args, _ElapsedTime)
 	local l_Owner = args["owner"]
 	local l_Enemy = m_CharacterManager.m_Enemics[1]
-	--l_Owner:set_position(l_Owner:get_position() + l_Owner:get_rotation():get_forward_vector() * _ElapsedTime * l_Enemy.m_Speed
-	--local l_NewControllerPosition = g_Player.m_PhysXManager:get_character_controler_pos("player")
+	local l_PlayerPos = g_Player.m_RenderableObject:get_position()
+	local l_EnemyPos = l_Owner:get_position()
+	local l_Direction = (l_PlayerPos - l_EnemyPos):get_normalized(1)
+
+
+	 
+	local l_EnemyForward = l_Owner:get_rotation():get_forward_vector():get_normalized(1)
+ 
+	local l_Angle = l_EnemyForward * l_Direction
+ 
+	local angle_to_turn = math.acos(l_Angle)
+	local cross = l_Direction ^ l_EnemyForward
+	if cross.y < 0.0 then
+	  angle_to_turn = -angle_to_turn
+	end
+	local quat_to_turn = Quatf()
+	quat_to_turn:quat_from_yaw_pitch_roll(angle_to_turn, 0.0, 0.0)
+	utils_log("angle: "..angle_to_turn)
+	local target_quat = l_Owner:get_rotation() * quat_to_turn
+	l_Owner:set_rotation(target_quat)
+	l_Owner:set_poisiton(l_Owner:get_position()+target_quat:get_forward_vector()*_ElapsedTime)
 end
 
 function FogChaseEnd(args)
-	utils_log("Chase end")
+	utils_log("end chase")
 end
 
 function FogChaseToTeleportCondition()
 	return false
 end
 
+function FogChaseToAttackCondition()
+--	local l_Enemy = m_CharacterManager.m_Enemics[1]
+--	local l_Direction = (l_PlayerPos - l_EnemyPos)
+--	l_Direction.y = 0
+--	return l_Direction:length() < 1
+return false
+end
+
 function FogChaseToOffCondition()
 	return m_CharacterManager.m_Enemics[1].m_off == true
 end
 
-function FogChaseToAttackCondition()
-	return false
-end
