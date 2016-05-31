9b25732a677d3dd62441dba7372494d2788ae704
Robb <trincasidra@users.noreply.github.com>
2016-05-31 19:18:09 +0200

    Color shadowmap.

--- MasterUAB/Graphics/Lights/Light.cpp
+++ MasterUAB/Graphics/Lights/Light.cpp
@@ -39,7 +39,7 @@ CLight::CLight(CXMLTreeNode &TreeNode) : CNamed(TreeNode), C3DElement(TreeNode)
 		}
 	}
 	if (m_GenerateShadowMap){
-		m_ShadowMap = new CDynamicTexture("shadowmap", TreeNode.GetFloatProperty("shadow_map_width"), TreeNode.GetFloatProperty("shadow_map_height"), true, "rgb8");
+		m_ShadowMap = new CDynamicTexture("shadowmap", TreeNode.GetFloatProperty("shadow_map_width"), TreeNode.GetFloatProperty("shadow_map_height"), true, "r32");
 		//m_ShadowMaskTexture = new CTexture();
 		m_ShadowMaskTexture = nullptr;
 	}
