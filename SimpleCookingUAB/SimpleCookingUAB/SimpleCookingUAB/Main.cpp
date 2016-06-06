cebfe1b3d0643758e4b89546955446f6567da7b6
Imdeeo <joanillo2002@gmail.com>
2016-06-06 20:05:41 +0200

    PhysXManager añadido Compila y ejecuta Falta que cree las mallas

--- SimpleCookingUAB/SimpleCookingUAB/SimpleCookingUAB/Main.cpp
+++ SimpleCookingUAB/SimpleCookingUAB/SimpleCookingUAB/Main.cpp
@@ -1,12 +1,16 @@
 #include "StaticMesh\StaticMeshManager.h"
+#include "PhyxManager\PhysXManager.h"
 
 void main()
 {
 	CStaticMeshManager l_StaticMehManager;
+
+	CPhysXManager * l_PhysXManager = CPhysXManager::CreatePhysXManager();
 
 	l_StaticMehManager.Load("Data\\static_meshes.xml");
 
 	char c;
 	printf("pene");
 	scanf("%c", &c);
+
 }
