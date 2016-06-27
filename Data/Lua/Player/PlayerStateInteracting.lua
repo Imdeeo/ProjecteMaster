267e4ad01b39a43aea5d538debdcffa32fa723ea
Ynozzar <aeyann@gmail.com>
2016-06-27 13:19:07 +0200

    Ahora la interacción con objetos funciona correctamente con Raycast. Para ClimbingState sigue funcionando con Triggers como antes.

--- Data/Lua/Player/PlayerStateInteracting.lua
+++ Data/Lua/Player/PlayerStateInteracting.lua
@@ -4,8 +4,9 @@ function InteractingFirst(args)
 	l_Player.m_IsClimbing = false
 	l_Owner:execute_action(l_Player.m_InteractingAnimation, 0.1, 0.1, 1.0, true)
 	if l_Player.m_InteractingCinematic ~= nil then
-		--CUABEngine.get_instance():get_cinematic_manager():play() --OJO! Lanzar cinemática.
+		--CUABEngine.get_instance():get_cinematic_manager():play() --OJO! Lanzar cinemática, y asignar m_AnimationTime.
 	end
+	m_AnimationTime = 2.5
 	m_Timer = 0.0
 	if l_Player.m_ItemName ~= nil then
 		l_Player.m_Item = CUABEngine.get_instance():get_layer_manager():get_resource("solid"):get_resource(l_Player.m_ItemName)
@@ -13,17 +14,14 @@ function InteractingFirst(args)
 end
 
 function InteractingUpdate(args, _ElapsedTime)
-utils_log("InteractingUpdate Start")
 	local l_Player = args["self"]
 	local l_Owner = args["owner"]
 	
+	--// Ends the state after the animation duration has passed
 	m_Timer = m_Timer + _ElapsedTime
-	
-	if m_Timer >= 2.5 then
+	if m_Timer >= m_AnimationTime then
 		l_Player.m_IsInteracting = false
 	end
-	utils_log("InteractingUpdate 1")
-	--l_Player.m_IsInteracting = l_Owner:is_action_animation_active(l_Player.m_InteractingAnimation)
 
 	--// Rotate player to match camera
 	l_RotationXZ = Quatf()
@@ -31,29 +29,30 @@ utils_log("InteractingUpdate Start")
 	l_Rotation = l_Player.m_CameraController:get_rotation()
 	l_Rotation:decouple_y(l_RotationXZ, l_RotationY)
 	l_Owner:set_rotation(l_RotationY)
-	utils_log("InteractingUpdate 2")
+
 	--// If player has an item, move it.
 	if m_Timer >= 1.5 and l_Player.m_Item ~= nil then
 		local l_NewControllerPosition = l_Player.m_PhysXManager:get_character_controler_pos("player")
 		l_NewControllerPosition.y = l_NewControllerPosition.y - 0.45
 		local l_ObjectPosition = l_Owner:get_rotation():rotated_vector(l_Owner:get_right_object_position())
-		--l_ObjectPosition = Vect3f(-l_ObjectPosition.x, l_ObjectPosition.z, l_ObjectPosition.y)
 		l_ObjectPosition.z = l_ObjectPosition.z * (-1.0)
 		l_ObjectPosition = l_ObjectPosition+l_NewControllerPosition
 		l_Player.m_Item:set_position(l_ObjectPosition)
 		local l_ObjectRotation = l_Owner:get_right_object_rotation()*l_Owner:get_rotation()
 		l_Player.m_Item:set_rotation(l_ObjectRotation)
 	end
-	utils_log("InteractingUpdate End")
 end
 
 function InteractingEnd(args)
 	local l_Player = args["self"]
 	local l_Owner = args["owner"]
 	l_Player.m_Target = nil
-	l_Player.m_IsCorrecting = false
+	l_Player.m_TargetOffset = Vect3f(1.0, 0.0, 0.0)
 	l_Player.m_InteractingAnimation = 0
 	l_Player.m_InteractingCinematic = nil
+	l_Player.m_IsInteracting = false
+	l_Player.m_IsClimbing = false
+	l_Player.m_IsCorrecting = false
 	l_Player.m_CameraController:unlock()
 	l_Owner:remove_action(l_Owner:get_actual_action_animation())
 end
