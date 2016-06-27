5a31bb37823357885a0ba1030ce7d0d1323dfb4f
Ynozzar <aeyann@gmail.com>
2016-06-27 13:28:20 +0200

    Simplificada la entrada a ClimbingState, ya no usa OnTriggerEnter. Preparado para que ClimbingState no siempre acabe por OnTriggerExit.

--- Data/Lua/Triggers.lua
+++ Data/Lua/Triggers.lua
@@ -10,16 +10,6 @@ function OnChapelStay(a, b)
 
 end
 
-function OnEnterClimbing(_TriggerName, _ColliderName)
-	local l_Player = m_CharacterManager.m_Player[1]
-	--TODO: Set l_Player.m_TargetOffset from trigger
-	l_Player.m_Target = CUABEngine.get_instance():get_layer_manager():get_resource("triggers"):get_resource(_TriggerName):get_position()
-	l_Player.m_TargetOffset = Vect3f(1.0, 0.0, 0.0)
-	l_Player.m_IsInteracting = false
-	l_Player.m_IsClimbing = true
-	l_Player.m_IsCorrecting = false
-end
-
 function OnExitClimbing(_TriggerName, _ColliderName)
 	local l_Player = m_CharacterManager.m_Player[1]
 	l_Player.m_IsInteracting = false
@@ -40,6 +30,9 @@ function CheckIfFacing(_Player, _TriggerName)
 	--// Then checks if angle between the vector triggerPos->playerPos and offset is less than 90 degrees (1.57 rads)
 	if (_Player.m_CameraController:get_forward():get_normalized(1):get_angle_with(_Player.m_TargetOffset) > 2.0 and (_Player.m_PhysXManager:get_character_controler_pos("player") - l_Target):get_normalized(1):get_angle_with(_Player.m_TargetOffset) < 1.57) then
 		_Player.m_Target = l_Target
+		_Player.m_TargetOffset = Vect3f(1.0, 0.0, 0.0)
+		_Player.m_IsInteracting = false
+		_Player.m_IsClimbing = true
 		_Player.m_IsCorrecting = true
 	end
 end
