f0edf7501b48ff0934dd60ddbb6e91f0f19e4254
jonathanhr <jonathan.hortelano@gmail.com>
2016-07-03 13:46:02 +0200

    El automata sigue con la mirada al player

--- Data/Lua/Enemies/CagedEnemy/CagedAutomatonStateIdle.lua
+++ Data/Lua/Enemies/CagedEnemy/CagedAutomatonStateIdle.lua
@@ -14,20 +14,18 @@ end
 
 function IdleUpdateCagedAutomaton(args, _ElapsedTime)
 	local l_Enemy = args["self"]
-	local l_Owner = args["owner"]
 	
 	l_Enemy.m_timer_to_stop = l_Enemy.m_timer_to_stop + _ElapsedTime
 	if l_Enemy.m_timer_to_stop >= 6.5 then
-		l_Owner:remove_action(0)
-		l_Owner:blend_cycle(2,1.0,0.5)
+		l_Enemy.m_State = "attack"
 	end
-	
-	-- Si la distancia entre el enemy y el player es menor a lo establecido pasamos a attack
-	local l_PlayerPos = g_Player.m_RenderableObject:get_position()
-	local l_Distance = l_Enemy.m_RenderableObject:get_position():distance(l_PlayerPos)
-	l_Enemy.m_Timer = l_Enemy.m_Timer + _ElapsedTime
-	l_Enemy:LoseSanity(l_Distance)
 end
 
 function IdleEndCagedAutomaton(args)
+	utils_log("IdleEndCagedAutomaton")
+end
+
+function IdleToAttackConditionCagedAutomaton(args)	
+	local l_Enemy = args["self"]
+	return l_Enemy.m_State == "attack"
 end
