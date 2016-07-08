4d451a6a4e0e5552e82a9745a549c2ebefd2557c
Paula <nah_one@hotmail.com>
2016-07-08 18:07:00 +0200

    testeo timers

--- MasterUAB/Core/Engine/UABEngine.cpp
+++ MasterUAB/Core/Engine/UABEngine.cpp
@@ -23,6 +23,7 @@
 #include "SoundManager\SoundManager.h"
 #include "GamePlayManager.h"
 #include "Manchas\ManchasManager.h"
+#include "DebugHelper\DebugHelper.h"
 
 CUABEngine::CUABEngine(void)
 {
@@ -105,6 +106,7 @@ void CUABEngine::Update(float _ElapsedTime)
 }
 void CUABEngine::Init()
 {
+	float l_StartTime = (float) timeGetTime();
 	LoadLevelXML("Data\\level.xml");
 	m_PhysXManager->LoadPhysx("Data\\physx.xml");
 	m_EffectManager->Load("Data\\effects.xml");
@@ -129,6 +131,12 @@ void CUABEngine::Init()
 
 	m_ScriptManager->RunFile("Data\\Lua\\init.lua");
 	m_ScriptManager->RunCode("mainLua(\""+m_LevelLoaded+"\")");
+	float l_EndTime = (float)timeGetTime();
+	float l_LoadTimer = l_EndTime - l_StartTime;
+	std::ostringstream ss;
+	ss << l_LoadTimer;
+	std::string s(ss.str());
+	CDebugHelper::GetDebugHelper()->Log(s);
 }
 void CUABEngine::Destroy()
 {
