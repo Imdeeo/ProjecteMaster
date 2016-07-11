60f84061940c31074624e92d930c3e11daf60509
jonathanhr <jonathan.hortelano@gmail.com>
2016-07-11 17:26:36 +0200

    incluyendo ffmpeg

--- MasterUAB/Video/VideoManager/VideoManagerImplementation.cpp
+++ MasterUAB/Video/VideoManager/VideoManagerImplementation.cpp
@@ -12,8 +12,6 @@ CVideoManagerImplementation::~CVideoManagerImplementation()
 
 bool CVideoManagerImplementation::Init()
 {
-	SDL_Surface *screen;
-
 	/* Initialize the SDL library */
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		fprintf(stderr,	"Couldn't initialize SDL: %s\n", SDL_GetError());
@@ -39,6 +37,14 @@ bool CVideoManagerImplementation::Init()
 	SDL_SetRenderDrawColor(sdlRenderer, 0, 0, 0, 255);
 	SDL_RenderClear(sdlRenderer);
 	SDL_RenderPresent(sdlRenderer);
+
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");  // make the scaled rendering look smoother.
+	SDL_RenderSetLogicalSize(sdlRenderer, 640, 480);
+
+	SDL_Texture *sdlTexture = SDL_CreateTexture(sdlRenderer,
+		SDL_PIXELFORMAT_ARGB8888,
+		SDL_TEXTUREACCESS_STREAMING,
+		640, 480);
 
 	return true;
 }
