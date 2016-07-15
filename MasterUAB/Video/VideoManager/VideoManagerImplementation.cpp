e2c1c1f865057bc2ed3c33e1cbb04ed2b9a36a3f
Paula <nah_one@hotmail.com>
2016-07-15 17:28:33 +0200

    vavava

--- MasterUAB/Video/VideoManager/VideoManagerImplementation.cpp
+++ MasterUAB/Video/VideoManager/VideoManagerImplementation.cpp
@@ -3,7 +3,7 @@
 
 CVideoManagerImplementation::CVideoManagerImplementation()
 {
-
+	mgr = new TheoraVideoManager();
 }
 
 CVideoManagerImplementation::~CVideoManagerImplementation()
@@ -13,18 +13,18 @@ CVideoManagerImplementation::~CVideoManagerImplementation()
 
 bool CVideoManagerImplementation::Init()
 {
-	mgr = new TheoraVideoManager();
 	clip = mgr->createVideoClip("Data\\Video\\file.ogg");
 	return true;
 }
  
 void CVideoManagerImplementation::Update(float _ElapsedTime)
 {
+	mgr->update(_ElapsedTime);
 	TheoraVideoFrame *frame = clip->getNextFrame();
 	if (frame)
 	{
 		// transfer the frame pixels to your display device, texure, graphical context or whatever you use.
 		clip->popFrame(); // be sure to pop the frame from the frame queue when you're done
 	}
-	mgr->update(_ElapsedTime);
+
 }
