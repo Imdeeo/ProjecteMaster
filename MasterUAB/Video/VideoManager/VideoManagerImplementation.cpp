c7073f03d90d7ec62f029fd7e33ecf4a5b468b75
Paula <nah_one@hotmail.com>
2016-07-17 17:50:34 +0200

    carga el video

--- MasterUAB/Video/VideoManager/VideoManagerImplementation.cpp
+++ MasterUAB/Video/VideoManager/VideoManagerImplementation.cpp
@@ -3,28 +3,29 @@
 
 CVideoManagerImplementation::CVideoManagerImplementation()
 {
-	mgr = new TheoraVideoManager();
+	theoraplayer::Manager* mgr = new theoraplayer::Manager();
+	theoraplayer::VideoClip* clip = mgr->createVideoClip("short.ogg");
 }
 
 CVideoManagerImplementation::~CVideoManagerImplementation()
 {
-	CHECKED_DELETE(mgr);
+	//CHECKED_DELETE(mgr);
 }
 
 bool CVideoManagerImplementation::Init()
 {
-	clip = mgr->createVideoClip("Data\\Video\\file.ogg");
+	
 	return true;
 }
  
 void CVideoManagerImplementation::Update(float _ElapsedTime)
 {
-	mgr->update(_ElapsedTime);
+	/*mgr->update(_ElapsedTime);
 	TheoraVideoFrame *frame = clip->getNextFrame();
 	if (frame)
 	{
 		// transfer the frame pixels to your display device, texure, graphical context or whatever you use.
 		clip->popFrame(); // be sure to pop the frame from the frame queue when you're done
-	}
+	}*/
 
 }
