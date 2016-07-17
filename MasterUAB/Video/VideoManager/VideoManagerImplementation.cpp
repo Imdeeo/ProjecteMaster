8235a2f4911311739c82798cef05e9623ee1287f
Paula <nah_one@hotmail.com>
2016-07-17 21:46:46 +0200

    Mierda final?

--- MasterUAB/Video/VideoManager/VideoManagerImplementation.cpp
+++ MasterUAB/Video/VideoManager/VideoManagerImplementation.cpp
@@ -1,29 +1,48 @@
 #include "VideoManagerImplementation.h"
 #include "Utils.h"
-
+#include "Engine\UABEngine.h"
+#include <theoraplayer/TheoraPlayer.h>
+#include <theoraplayer/TheoraDataSource.h>
+#include "RenderManager\RenderManager.h"
+#include "ContextManager\ContextManager.h"
+#include "Texture\DynamicTexture.h"
+#include "RenderableObjects\RenderableObjectTechnique.h"
+#include "RenderableObjects\RenderableObjectTechniqueManager.h"
+#include "D3DX11tex.h"
 CVideoManagerImplementation::CVideoManagerImplementation()
 {
-	mgr = new theoraplayer::Manager();	
+	mgr = new TheoraVideoManager();
 }
 
 CVideoManagerImplementation::~CVideoManagerImplementation()
 {
-	CHECKED_DELETE(mgr);
+	//CHECKED_DELETE(mgr);
 }
 
 bool CVideoManagerImplementation::Init()
 {
-	clip = mgr->createVideoClip("short.ogg");
+	clip = mgr->createVideoClip("media/bunny.ogv");
+	if (clip == nullptr)
+		return false;
 	return true;
 }
  
 void CVideoManagerImplementation::Update(float _ElapsedTime)
 {
 	mgr->update(_ElapsedTime);
-	theoraplayer::VideoFrame *frame = clip->fetchNextFrame();
+	TheoraVideoFrame *frame = clip->getNextFrame();
 	if (frame)
 	{
-		// transfer the frame pixels to your display device, texure, graphical context or whatever you use.
+		//ID3D11ShaderResourceView *l_Texture;
+		//D3DXCreateTexture(device, size.x, size.y, numMipMaps, usage, textureFormat, D3DPOOL_MANAGED, &l_Texture);
+		//CDynamicTexture *l_Texture = new CDynamicTexture("pepe", clip->getWidth(), frame->getHeight(), false, "rgba8");
+		//l_Texture->m_Texture = (ID3D11ShaderResourceView*)frame->getBuffer();
+		//CRenderableObjectTechnique* l_RenderableObjectTechnique = UABEngine.GetRenderableObjectTechniqueManager()->GetResource("MV_POSITION4_NORMAL_TEXTURE_VERTEX");
+
+		//UABEngine.GetRenderManager()->DrawScreenQuad(l_RenderableObjectTechnique->GetEffectTechnique(), l_Texture, 0, 0, 1, 1, CColor(1.f, 1.f, 1.f, 1.f));
+		//glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, clip->getWidth(), frame->getHeight(), textureFormat, GL_UNSIGNED_BYTE, frame->getBuffer());
+
+			// transfer the frame pixels to your display device, texure, graphical context or whatever you use.
 		clip->popFrame(); // be sure to pop the frame from the frame queue when you're done
 	}
 
