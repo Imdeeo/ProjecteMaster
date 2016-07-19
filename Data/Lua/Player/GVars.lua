8578e0e860de79b67f90bd93bac44bbc1000a9ed
Ynozzar <aeyann@gmail.com>
2016-07-19 15:33:36 +0200

    Corregido el bug de la altura del personaje.

---
+++ Data/Lua/Player/GVars.lua
@@ -0,0 +1,17 @@
+--// 
+--// Total character height is g_Height+(2*g_Radius), so here it will be 1.8
+--// 	 ____
+--//	/    \  -> g_Radius		]
+--//	|	 |	}				]
+--//	|	 |	}				]  TOTAL
+--//	|	 |	}  g_Height		] 
+--//	|	 |	}				]  HEIGHT
+--//	|	 |	}				]
+--//	\____/  -> g_Radius		]
+--// 
+
+g_Height = 1.2
+g_Radius = 0.3
+g_CrouchingHeight = 0.4
+g_StandingOffset = (g_Height*0.5)+g_Radius
+g_CrouchingOffset = (g_CrouchingHeight*0.5)+g_Radius
