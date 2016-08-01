1ff734f3822754294768b28291bc3879665ad3de
jonathanhr <jonathan.hortelano@gmail.com>
2016-08-01 19:20:13 +0200

    arreglando bugs

--- MasterUAB/Graphics/Cinematics/CinematicObject.cpp
+++ MasterUAB/Graphics/Cinematics/CinematicObject.cpp
@@ -140,7 +140,7 @@ void CCinematicObject::OnRestartCycle()
 void CCinematicObject::GetCurrentKey()
 {
 	m_CurrentKeyFrame = 0;
-	for (size_t i = 0; i < m_CinematicObjectKeyFrames.size()-1; i++){
+	for (size_t i = 0; i < m_CinematicObjectKeyFrames.size(); i++){
 		if (m_CurrentTime >= m_CinematicObjectKeyFrames[i]->GetKeyFrameTime())
 		{
 			m_CurrentKeyFrame = i;
