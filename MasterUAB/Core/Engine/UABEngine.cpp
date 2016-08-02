c484899be70e66364d08ba1af66da25491c8c539
Paula <nah_one@hotmail.com>
2016-08-02 17:18:06 +0200

    Solo se carga un nivel al principio

--- MasterUAB/Core/Engine/UABEngine.cpp
+++ MasterUAB/Core/Engine/UABEngine.cpp
@@ -118,7 +118,7 @@ void CUABEngine::Init()
 	m_RenderableObjectTechniqueManager->Load("Data\\renderable_objects_techniques.xml");
 	m_AnimatedModelsManager->Load("Data\\animated_models.xml");
 	m_LevelManager->LoadLevel("Recibidor");
-	m_LevelManager->LoadLevel("Biblioteca");
+	//m_LevelManager->LoadLevel("Biblioteca");
 	m_GUIManager->Load("Data\\GUI\\gui_elements.xml");
 	m_ScriptManager->Initialize();
 	m_MaterialManager->Load("Data\\default_effect_materials.xml");
