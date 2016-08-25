f7167de20fb067bf9a8a2c303c2d50e7e1971e8d
Robb <trincasidra@users.noreply.github.com>
2016-08-25 12:06:39 +0200

    Corregida reubicación angular en correctingstate

--- Data/Lua/Raycast.lua
+++ Data/Lua/Raycast.lua
@@ -23,7 +23,7 @@ function FacingRaycast(_Player, _Target, _Pos, _Distance)
 	--utils_log("Pos: ".._Pos.x..", ".._Pos.y..", ".._Pos.z)
 	--utils_log("Target: ".._Target.x..", ".._Target.y..", ".._Target.z)
 	--utils_log("Offset: ".._Player.m_TargetOffset.x..", ".._Player.m_TargetOffset.y..", ".._Player.m_TargetOffset.z)
-	utils_log("Distance: "..(_Pos - _Target):length())
+	--utils_log("Distance: "..(_Pos - _Target):length())
 	return ((_Pos - _Target):length() < _Distance)
 end
 
