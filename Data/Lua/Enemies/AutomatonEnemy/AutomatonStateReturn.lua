60f578eaecfb6f1723138326064b4c4213da8114
jonathanhr <jonathan.hortelano@gmail.com>
2016-09-14 17:50:16 +0200

    - mejorando maldito automata

--- Data/Lua/Enemies/AutomatonEnemy/AutomatonStateReturn.lua
+++ Data/Lua/Enemies/AutomatonEnemy/AutomatonStateReturn.lua
@@ -13,6 +13,7 @@ function ReturnFirstAutomaton(args)
 	l_Enemy.m_TimerRotation2 = 0.0
 	l_Enemy.m_IsReturn = false
 	l_Enemy.m_LastPositionPlayer = nil
+	l_Enemy.m_IsCorrected = false
 end
 
 function ReturnUpdateAutomaton(args, _ElapsedTime)
@@ -40,24 +41,59 @@ function ReturnUpdateAutomaton(args, _ElapsedTime)
 		-- Comprobamos si ha podido establecer una ruta
 		if l_Enemy.m_TotalNodes > 0 then
 			-- Actualizamos la posicion del enemigo
-			local l_PointPos = l_Enemy.m_PathFinding:get_point(l_Enemy.m_Name, l_Enemy.m_IndexPoint)
-			l_Enemy:EnemyWalk(l_PointPos, l_Enemy.m_WalkSpeed, l_PercentRotation, _ElapsedTime)
-			
-			-- Si la distancia entre el enemy y el punto es menor de un valor predeterminado pasamos al siguiente punto
-			local l_Distance = l_Enemy.m_RenderableObject:get_position():distance(l_PointPos)	
-			if l_Distance <= l_Enemy.m_DistanceToChangeNodeWalking then
-				if l_Enemy:IncrementPathPointIndex() == false then
-					local angle_to_turn = l_Enemy:CalculateAngleRotation(l_Owner:get_rotation():get_forward_vector(), l_Enemy.m_DefaultForward)
-					if angle_to_turn ~= nil and (angle_to_turn > 0.75 or angle_to_turn < 0.65) then
-						l_Enemy.m_TimerRotation2 = l_Enemy.m_TimerRotation2 + _ElapsedTime	
-						l_PercentRotation = l_Enemy.m_TimerRotation2 / l_Enemy.m_AngularWalkSpeed * 5
-							
-						if l_PercentRotation > 1.0 then
-							l_PercentRotation = 1.0
+			if not l_Enemy.m_IsCorrected then
+				if l_Enemy.m_IndexPoint == 0 then
+					-- primera correccion				
+					local l_FirstPointPos = l_Enemy.m_PathFinding:get_point(l_Enemy.m_Name, l_Enemy.m_IndexPoint)
+					local l_SecondPointPos = l_Enemy.m_PathFinding:get_point(l_Enemy.m_Name, l_Enemy.m_IndexPoint+1)
+					local l_EnemyPos = l_Owner:get_position()
+					local l_FirstDirection = (l_FirstPointPos - l_EnemyPos)
+					l_FirstDirection.y = 0
+					l_FirstDirection:normalize(1)
+					local l_SecondDirection = (l_EnemyPos - l_SecondPointPos)
+					l_SecondDirection.y = 0
+					l_SecondDirection:normalize(1)
+					
+					local angle_to_turn = l_Enemy:CalculateAngleRotation(l_FirstDirection, l_SecondDirection)
+					
+					if angle_to_turn == nil or l_FirstPointPos:distance(l_EnemyPos) <= l_Enemy.m_DistanceToChangeNodeRunning then
+						utils_log("INCREMENTANDO NODO!!!")
+						l_Enemy:IncrementPathPointIndex()
+					end
+				end
+				
+				local l_PointPos = l_Enemy.m_PathFinding:get_point(l_Enemy.m_Name, l_Enemy.m_IndexPoint)
+				local l_Direction = (l_PointPos - l_Owner:get_position())
+				l_Direction.y = 0
+				l_Direction:normalize(1)
+				
+				local angle_to_turn = l_Enemy:CalculateAngleRotation(l_Owner:get_rotation():get_forward_vector(), l_Direction)
+				
+				if angle_to_turn ~= nil then
+					l_Enemy:EnemyRotation(angle_to_turn, _ElapsedTime)
+				else
+					l_Enemy.m_IsCorrected = true
+				end
+			else
+				local l_PointPos = l_Enemy.m_PathFinding:get_point(l_Enemy.m_Name, l_Enemy.m_IndexPoint)
+				l_Enemy:EnemyWalk(l_PointPos, l_Enemy.m_WalkSpeed, l_PercentRotation, _ElapsedTime)
+				
+				-- Si la distancia entre el enemy y el punto es menor de un valor predeterminado pasamos al siguiente punto
+				local l_Distance = l_Enemy.m_RenderableObject:get_position():distance(l_PointPos)	
+				if l_Distance <= l_Enemy.m_DistanceToChangeNodeWalking then
+					if l_Enemy:IncrementPathPointIndex() == false then
+						local angle_to_turn = l_Enemy:CalculateAngleRotation(l_Owner:get_rotation():get_forward_vector(), l_Enemy.m_DefaultForward)
+						if angle_to_turn ~= nil and (angle_to_turn > 0.75 or angle_to_turn < 0.65) then
+							l_Enemy.m_TimerRotation2 = l_Enemy.m_TimerRotation2 + _ElapsedTime	
+							l_PercentRotation = l_Enemy.m_TimerRotation2 / l_Enemy.m_AngularWalkSpeed * 5
+								
+							if l_PercentRotation > 1.0 then
+								l_PercentRotation = 1.0
+							end
+							l_Enemy:EnemyRotation(angle_to_turn, l_PercentRotation)
+						else
+							l_Enemy.m_State = "idle"
 						end
-						l_Enemy:EnemyRotation(angle_to_turn, l_PercentRotation)
-					else
-						l_Enemy.m_State = "idle"
 					end
 				end
 			end
