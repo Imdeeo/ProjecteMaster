4fb99a320c290835571399922c4ff660ae528b1c
jonathanhr <jonathan.hortelano@gmail.com>
2016-10-21 13:40:26 +0200

    - cambios de materiales y renderables para level_3 y 2 - mejorado sistema de muerte de los enemigos

--- Data/Lua/Enemies/AutomatonEnemy/AutomatonStateKill.lua
+++ Data/Lua/Enemies/AutomatonEnemy/AutomatonStateKill.lua
@@ -15,7 +15,10 @@ function KillFirstAutomaton(args)
 	l_Owner:set_rotation(auxQuatf)
 	g_Player:SetAnimationCamera("JaheemDies2")
 	
+	l_Enemy.m_Timer = 0
 	l_Enemy.m_TimerToStop = 0
+	
+	l_Enemy.m_LoseSanityValue = g_Player.m_Sanity / 6.0
 	
 	l_Enemy:ShowParticles("EmisorParticulaTorreta", true)
 end
@@ -25,18 +28,16 @@ function KillUpdateAutomaton(args, _ElapsedTime)
 	local l_Owner = args["owner"]
 	local l_PlayerPos = g_Player.m_RenderableObject:get_position()
 	
-	l_Enemy.m_Timer = l_Enemy.m_Timer + _ElapsedTime
 	l_Enemy.m_TimerToStop = l_Enemy.m_TimerToStop + _ElapsedTime
-	if l_Enemy.m_TimerToStop >= 5.5 then
-		l_Enemy.m_State = "off"
+	l_Enemy.m_Timer = l_Enemy.m_Timer + _ElapsedTime
+	
+	if l_Enemy.m_Timer >= 1.0 then
+		g_Player:ModifySanity(-l_Enemy.m_LoseSanityValue)
+		l_Enemy.m_Timer = l_Enemy.m_Timer - 1.0
 	end
 	
-	if g_Player.m_Sanity > 0 then
-		local l_LoseSanityValue = g_Player.m_Sanity / (6.0 - l_Enemy.m_TimerToStop)
-		if l_Enemy.m_Timer >= 0.5 then
-			g_Player:ModifySanity(-l_LoseSanityValue)
-			l_Enemy.m_Timer = 0
-		end
+	if l_Enemy.m_TimerToStop >= 6.5 then
+		l_Enemy.m_State = "off"
 	end
 	
 	l_Enemy:ShowParticles("EmisorParticulaTorreta", false)
@@ -50,10 +51,9 @@ function KillEndAutomaton(args)
 	l_CameraManager:choose_main_camera("MainCamera")
 	l_CameraManager:get_main_camera():unlock()
 	l_Owner:set_rotation(l_Enemy.m_DefaultRotation)
-	local UABEngine = CUABEngine.get_instance()
-	local l_TurretParticle = UABEngine:get_layer_manager():get_layer("particles"):get_resource("EmisorParticulaTorreta")
-	l_TurretParticle:set_visible(false)
-	l_TurretParticle:set_start(false)
+	local l_AutomatonParticle = g_Engine:get_level_manager():get_level(g_Player.m_ActualLevel):get_layer_manager():get_layer("particles"):get_resource("EmisorParticulaTorreta")
+	l_AutomatonParticle:set_visible(false)
+	l_AutomatonParticle:set_start(false)
 end
 
 function KillToOffConditionAutomaton(args)
