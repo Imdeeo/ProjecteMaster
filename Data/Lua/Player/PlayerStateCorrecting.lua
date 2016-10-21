5573b9fc59b122fbec0be93b4ed7c95e5ecb20a1
Robb <trincasidra@users.noreply.github.com>
2016-10-21 12:43:11 +0200

    Correcciones merge

--- Data/Lua/Player/PlayerStateCorrecting.lua
+++ Data/Lua/Player/PlayerStateCorrecting.lua
@@ -1,7 +1,7 @@
 function CorrectingFirst(args)
 	local l_Player = args["self"]
 	l_Player.m_CameraController:lock()
-	--l_Player:CalculateCameraPositionRotation(l_Player.m_CameraAnimation, l_Player.m_TargetPosOffset)
+
 	l_Player.m_InitialCameraRotation = l_Player.m_CameraController:get_rotation()
 	l_Player.m_TimerRotation = 0.0
 
@@ -16,39 +16,7 @@ end
 
 function CorrectingUpdate(args, _ElapsedTime)
 	local l_Player = args["self"]
-	local l_Owner = args["owner"]
-	
-	--[[local l_AngleOK = false
-	local l_PosOK = false
-	
-	l_Player.m_TimerRotation = l_Player.m_TimerRotation + _ElapsedTime
-	local l_PercentRotation = l_Player.m_TimerRotation 
-	if l_PercentRotation > 1.0 then
-		l_PercentRotation = 1.0
-	end
-	
-	utils_log_v3(l_Player.m_Target)
-	local l_FaceTargetDisplacement =  l_Player.m_Target - l_Player.m_PhysXManager:get_character_controler_pos("player")
-	l_FaceTargetDisplacement.y = 0.0
-		
-	if l_FaceTargetDisplacement:length() <= 0.01 then
-		l_PosOK = true
-	else
-		l_Player.m_PhysXManager:character_controller_move("player", l_FaceTargetDisplacement:get_normalized(1), _ElapsedTime)
-	end		
-	
-	if l_Player.m_TimerRotation <= 1.0 then
-		local target_quat = l_Player.m_InitialCameraRotation:slerpJU(l_Player.m_FinalCameraRotation, l_PercentRotation)
-		l_Player.m_CameraController:set_rotation(target_quat)
-	else
-		l_AngleOK = true
-	end		
-
-	if l_PosOK and l_AngleOK then
-		l_Player.m_IsCorrecting = false
-		l_Player.m_IsInteracting = true
-	end]]
-	
+	local l_Owner = args["owner"]	
 	
 	local l_AngleOK = false
 	local l_PosOK = false
@@ -73,43 +41,6 @@ function CorrectingUpdate(args, _ElapsedTime)
 	end
 	
 		--// Rotation
-	--[[local l_CameraDirection = l_Player.m_CameraController:get_forward()
-	l_CameraDirection.y = 0.0
-	l_CameraDirection:normalize(1)
-	local l_Off = l_Player.m_TargetLookOffset
-	l_Off = l_Off * (-1.0)
-	l_Off.y = 0.0
-	l_Off:normalize(1)
-	local l_Yaw = l_CameraDirection:get_angle_with(l_Off)
-	local l_Dir = 1
-	local l_OriginYaw
-	utils_log_v3(l_Off)
-	utils_log("atan2: "..math.atan2(l_CameraDirection.z, l_CameraDirection.x))
-	if math.abs(l_Off.x) > math.abs(l_Off.z) then
-		l_OriginYaw = math.atan2(l_CameraDirection.z, l_CameraDirection.x)
-		if l_OriginYaw > 0 then
-			l_Dir = -l_Dir
-		end
-		if (l_Off.x < 0) then
-			l_Dir = -l_Dir
-		end
-	else
-		l_OriginYaw = math.atan2(l_CameraDirection.x, l_CameraDirection.z)
-		if l_OriginYaw < 0 then
-			l_Dir = -l_Dir
-		end
-		if (l_Off.x > 0) then
-			l_Dir = -l_Dir
-		end
-	end
-	
-	if l_Yaw <= 0.01 then
-		l_AngleOK = true
-	else
-		l_Player.m_CameraController:add_yaw(l_Yaw * _ElapsedTime)
-	end]]
-		
-
 	l_Player.m_TimerRotation = l_Player.m_TimerRotation + _ElapsedTime
 	local l_PercentRotation = l_Player.m_TimerRotation 
 	if l_PercentRotation > 1.0 then
@@ -127,24 +58,22 @@ function CorrectingUpdate(args, _ElapsedTime)
 		
 		
 	if l_PosOK and l_AngleOK then
-		--l_Player.m_PhysXManager:character_controller_move("player", l_FaceTargetDisplacement, _ElapsedTime)
-		--l_Player.m_CameraController:add_yaw(l_Yaw)
 		l_Player.m_IsCorrecting = false
 	end]]
 	
-	--// Assign to the character the controller's position
+		--// Assign to the character the controller's position
 	local l_NewControllerPosition = l_Player.m_PhysXManager:get_character_controler_pos("player")
 	l_NewControllerPosition.y = l_NewControllerPosition.y - g_StandingOffset
 	l_Owner:set_position(l_NewControllerPosition)
 
-	--// Rotate player to match camera
+		--// Rotate player to match camera
 	l_RotationXZ = Quatf()
 	l_RotationY = Quatf()
 	l_Rotation = l_Player.m_CameraController:get_rotation()
 	l_Rotation:decouple_y(l_RotationXZ, l_RotationY)
 	l_Owner:set_rotation(l_RotationY)
 	
-	--// If player has an item, move it.
+		--// If player has an item, move it.
 	if l_Player.m_Item ~= nil then
 		local l_CameraDirection = l_Player.m_CameraController:get_forward()
 		local l_ObjectPosition
@@ -189,6 +118,5 @@ end
 
 function CorrectingToInteractingCondition(args)
 	local l_Player = args["self"]
-	--return false
 	return l_Player.m_IsInteracting and not l_Player.m_IsCorrecting
 end
