0ee0407c80d383d8fbf8e716002debd6ecb87991
Ynozzar <aeyann@gmail.com>
2016-10-22 18:47:45 +0200

    Corregidos Valve e InputValve
    Separada la caldera en caldera+inputpipe

--- Data/Lua/Raycast/Maquinas.lua
+++ Data/Lua/Raycast/Maquinas.lua
@@ -6,33 +6,39 @@ local l_LevelId = "Maquinas"
 
 function R3Valve(_Player)
 	_Player.m_TargetYaw = g_PI
-	_Player.m_TargetPosOffset = Vect3f(-1.0, 0.0, 0.0)
+	_Player.m_ForwardCamera = Vect3f(0.766045, -0.642787, 0.0)
+	_Player.m_UpCamera = Vect3f(0.642787, 0.766045, 0.0)
+	_Player.m_TargetPosOffset = Vect3f(-1.65, 0.0, 0.0)
 	l_Target = GetTriggerPos("TriggerValve", l_LevelId)
 	if _Player:IsFacingTarget(l_Target, 1.0, 2.0) then
 		_Player.m_Target = l_Target
-		_Player.m_InteractingAnimation = 23
+		_Player.m_InteractingAnimation = 24
 		_Player.m_InteractingCinematic = nil
 		_Player.m_AnimationTime = 7.3333333
-		_Player.m_CameraAnimation = nil--"PickValve"
+		_Player.m_CameraAnimation = "PickValve"
 		_Player.m_NewItemName = "ValvulaPuzzle"
 		_Player.m_ItemTime = 5.0
 		_Player.m_ItemDropTime = -1.0
 		_Player.m_CurrentAend = nil
-		_Player.m_LeftHanded = false
+		_Player.m_LeftHanded = true
 		_Player.m_IsInteracting = true
 		_Player.m_IsCorrecting = true
 		_Player.m_IsPuzzle = false
+		--_Player.m_PhysXManager:enable_trigger("TriggerValveInput")
+		--_Player.m_PhysXManager:disable_trigger("TriggerValve")
 		--Play Sound¿?
 	end
 end
 
 function R3ValveInput(_Player)
 	_Player.m_TargetYaw = -g_PI/4.0
-	_Player.m_TargetPosOffset = Vect3f(1.0, 0.0, -1.0)
+	_Player.m_ForwardCamera = Vect3f(-0.5, 0.0, 0.866)
+	_Player.m_UpCamera = Vect3f(0.0, 1.0, 0.0)
+	_Player.m_TargetPosOffset = Vect3f(0.4, 0.0, -0.6928)
 	l_Target = GetTriggerPos("TriggerValveInput", l_LevelId)
 	if _Player:IsFacingTarget(l_Target, 1.0, 1.2) then
 		_Player.m_Target = l_Target
-		_Player.m_InteractingAnimation = 24
+		_Player.m_InteractingAnimation = 25
 		_Player.m_InteractingCinematic = nil
 		_Player.m_AnimationTime = 6.666667
 		_Player.m_CameraAnimation = nil--"InputValve"
@@ -46,16 +52,20 @@ function R3ValveInput(_Player)
 		_Player.m_IsPuzzle = false
 		-- Play Sound
 		R3ValveIsPlaced = true
+		_Player.m_CinematicManager:get_resource("ResolveValve"):play()
+		--_Player.m_PhysXManager:disable_trigger("TriggerValveInput")
 	end
 end
 
 function R3ValveDoor(_Player)
 	_Player.m_TargetYaw = -g_PI/2.0
+	_Player.m_ForwardCamera = Vect3f(1.0, 0.0, 0.0)
+	_Player.m_UpCamera = Vect3f(0.0, 1.0, 0.0)
 	_Player.m_TargetPosOffset = Vect3f(-1.0, 0.0, 1.0)
 	l_Target = GetTriggerPos("TriggerValveDoor", l_LevelId)
 	if _Player:IsFacingTarget(l_Target, 1.0, 1.0) then
 		_Player.m_Target = l_Target
-		_Player.m_InteractingAnimation = 25
+		_Player.m_InteractingAnimation = 26
 		_Player.m_InteractingCinematic = "CrossValveDoor"
 		_Player.m_CameraAnimation = "CrossValveDoor"
 		_Player.m_ItemDropTime = -1.0
